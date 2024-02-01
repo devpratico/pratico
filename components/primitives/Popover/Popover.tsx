fae762f6a6b66dc607346cdc9bcaf884e32ccf3d
Thomas DOUCHE <Thomas.douche@aol.com>
2024-02-01 17:41:14 +0100

    New Popover component

---
+++ components/primitives/Popover/Popover.tsx
@@ -0,0 +1,45 @@
+"use client"
+import React from 'react';
+import styles from './Popover.module.css'
+import * as PopoverPrimitive from '@radix-ui/react-popover'
+
+
+interface PopoverProps {
+    children: React.ReactNode;
+    content: React.ReactNode;
+    side?: "top" | "right" | "bottom" | "left";
+    align?: "start" | "center" | "end";
+    defaultOpen?: boolean;
+    open?: boolean;
+    onOpenChange?: (open: boolean) => void;
+    modal?: boolean;
+    arrow?: boolean;
+}
+
+
+export default function Popover({ children, content, side="right", align="center", defaultOpen=false, open, onOpenChange, modal=false, arrow=false }: PopoverProps) {
+    return (
+        <PopoverPrimitive.Root open={open} onOpenChange={onOpenChange} defaultOpen={defaultOpen} modal={modal}>
+
+            <PopoverPrimitive.Trigger className={styles.trigger} asChild>
+                {children}
+            </PopoverPrimitive.Trigger>
+
+
+            <PopoverPrimitive.Portal>
+                <PopoverPrimitive.Content side={side} className={styles.content} align={align}>
+
+                    { arrow && <PopoverPrimitive.Arrow className={styles.arrow} /> }
+
+                    <div className={styles.contentInner}>
+                        <PopoverPrimitive.Close className={styles.close} />
+                    </div>
+
+                    {content}
+
+                </PopoverPrimitive.Content>
+            </PopoverPrimitive.Portal>
+
+        </PopoverPrimitive.Root>
+    )
+}
