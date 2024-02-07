bf7659afb07e0997d6713a0676d9eac1ab9f6c03
Thomas DOUCHE <Thomas.douche@aol.com>
2024-02-07 17:48:28 +0100

    Locale redirection

--- middlewares/intlMiddleware.ts
+++ middlewares/intlMiddleware.ts
@@ -8,13 +8,29 @@ import { NextResponse, type NextRequest } from 'next/server'
  */
 export default function intlMiddleware(request: NextRequest) {
 
+    /**
+    * Bypass middleware for specific paths:
+    * - _next/static (static files)
+    * - _next/image (image optimization files)
+    * - favicon.ico (favicon file)
+    * Feel free to modify this pattern to include more paths.
+    * @see https://next-intl-docs.vercel.app/docs/routing/middleware#matcher-no-prefix
+    */
+    const bypassPathsRegex = /^\/(_next\/static|_next\/image|favicon.ico)/
+    if (bypassPathsRegex.test(request.nextUrl.pathname)) {
+        return NextResponse.next()
+    }
+
+    /*
     // Match only internationalized pathnames
-    const internationalizedPathnameRegex = /^\/(en|fr)(\/|$)/ ///^\/(de|en)($|\/.*)/
+    //const internationalizedPathnameRegex = /^\/(en|fr)(\/|$)/
+    const internationalizedPathnameRegex = /^\/(fr|en)($|\/.*)/
 
     // If it doesn't match, do nothing
     if (!internationalizedPathnameRegex.test(request.nextUrl.pathname)) {
         return NextResponse.next()
     }
+    */
 
     // Otherwise, apply the middleware
     return nextIntlMiddleware(request)
@@ -25,5 +41,5 @@ const nextIntlMiddleware = createMiddleware({
     // Define the supported locales
     locales: ['en', 'fr'],
     // Define the default locale
-    defaultLocale: 'en',
+    defaultLocale: 'fr',
 })
--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 const createNextIntlPlugin = require('next-intl/plugin')
-const withNextIntl = createNextIntlPlugin()
+const withNextIntl = createNextIntlPlugin(
+    './intl/i18n.ts'
+)
 
 
 /** @type {import('next').NextConfig} */
