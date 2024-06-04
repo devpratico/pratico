8b01a68c80b2990b060b06e19d2f305b30a45032
Thomas DOUCHE <Thomas.douche@aol.com>
2024-06-04 17:55:51 +0200

    tooltip

--- app/[locale]/room/[room_code]/@teacherView/_components/QrButton.tsx
+++ app/[locale]/room/[room_code]/@teacherView/_components/QrButton.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { IconButton } from "@radix-ui/themes";
+import { IconButton, Tooltip } from "@radix-ui/themes";
 import { QrCode } from 'lucide-react';
 import { useMenus } from "@/app/[locale]/_hooks/useMenus";
 
@@ -9,9 +9,11 @@ export default function QrButton() {
     const { setOpenDeskMenu } = useMenus()
 
     return (
-        <IconButton onClick={() => setOpenDeskMenu('qr')}>
-            <QrCode />
-        </IconButton>
+        <Tooltip content='QR Code'>
+            <IconButton onClick={() => setOpenDeskMenu('qr')} variant='ghost' style={{color:'var(--background)'}}>
+                <QrCode />
+            </IconButton>
+        </Tooltip>
     )
 
 }
