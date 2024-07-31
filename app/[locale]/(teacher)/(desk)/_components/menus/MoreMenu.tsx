0f01579fa3e0dbfc48d4508ac5f898e68f9ef6e0
Thomas DOUCHE <Thomas.douche@aol.com>
2024-07-31 10:50:06 +0200

    different more menu for session

--- app/[locale]/(teacher)/(desk)/_components/menus/MoreMenu.tsx
+++ app/[locale]/(teacher)/(desk)/_components/menus/MoreMenu.tsx
@@ -1,14 +1,36 @@
+'use client'
 import { Section, Callout, Box, Flex } from '@radix-ui/themes'
 import StartBtn from '../../capsule/[capsule_id]/_components/StartBtn'
+import { useParams } from 'next/navigation'
+
+/**
+ * Content of the 'More' menu that only shows on 'creation' phase (not during session)
+ */
+function CreationMenu() {
+    return (
+        <Flex display={{ initial: 'flex', xs: 'none' }} justify='center'>
+            <StartBtn message='lancer la session' variant='solid' />
+        </Flex>
+    )
+}
+
+
+/**
+ * Content of the 'More' menu that only hsows during the session
+ */
+function AnimationMenu() {
+    return null
+}
 
 
 export default function MoreMenu() {
+    const params = useParams()
+    const isRoom = params.room_code !== undefined
 
     return (
         <Section size='1'>
-            <Flex display={{ initial: 'flex', xs: 'none' }} justify='center'>
-                <StartBtn message='lancer la session' variant='solid'/>
-            </Flex>
+
+            {isRoom ? <AnimationMenu /> : <CreationMenu />}
 
             <Box display={{ initial: 'none', xs: 'block' }}>
                 <Callout.Root variant='outline'>
