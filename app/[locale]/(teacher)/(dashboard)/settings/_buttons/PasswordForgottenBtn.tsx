969f95cafa400751af6a4d3a866858f9d6b74382
Johanna <jcourtoi@student.fr>
2024-09-05 09:34:04 +0200

    COMMENT the email way to send a notif

--- app/[locale]/(teacher)/(dashboard)/settings/_buttons/PasswordForgottenBtn.tsx
+++ app/[locale]/(teacher)/(dashboard)/settings/_buttons/PasswordForgottenBtn.tsx
@@ -1,40 +1,40 @@
-import React, { useState } from 'react';
-import { Resend } from 'resend';
-import { Button } from '@radix-ui/themes';
-import { EmailTemplate } from '../notifications/EmailTemplate';
+// import React, { useState } from 'react';
+// import { Resend } from 'resend';
+// import { Button } from '@radix-ui/themes';
+// import { EmailTemplate } from '../notifications/EmailTemplate';
 
-const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
+// const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 
-export default function PasswordForgottenBtn() {
-  const [message, setMessage] = useState('');
+// export default function PasswordForgottenBtn() {
+//   const [message, setMessage] = useState('');
 
-  const handleSendEmail = async (event: React.FormEvent) => {
-    event.preventDefault();
+//   const handleSendEmail = async (event: React.FormEvent) => {
+//     event.preventDefault();
     
-    try {
-      const result = await fetch('/api/resend', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${process.env.NEXT_PUBLIC_RESEND_API_KEY}`
-        },
-        body: JSON.stringify({
-          from: 'lemaildejo@gmail.com',
-          to: ['jcourtoi@student.42.fr'],
-          subject: 'Email test',
-          react: EmailTemplate({ name: "Johanna" })
-        }),
-      });
+//     try {
+//       const result = await fetch('/api/resend', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//           'Authorization': `Bearer ${process.env.NEXT_PUBLIC_RESEND_API_KEY}`
+//         },
+//         body: JSON.stringify({
+//           from: 'lemaildejo@gmail.com',
+//           to: ['jcourtoi@student.42.fr'],
+//           subject: 'Email test',
+//           react: EmailTemplate({ name: "Johanna" })
+//         }),
+//       });
 
-      const data = await result.json();
-      setMessage(data.message);
-    } catch (error) {
-      console.error('Error while sending message', error);
-      setMessage('Error while sending msg.');
-    }
-  };
+//       const data = await result.json();
+//       setMessage(data.message);
+//     } catch (error) {
+//       console.error('Error while sending message', error);
+//       setMessage('Error while sending msg.');
+//     }
+//   };
 
-  return (
-      <Button onClick={handleSendEmail}>Mot de passe oublié</Button>
-  );
-};
+//   return (
+//       <Button onClick={handleSendEmail}>Mot de passe oublié</Button>
+//   );
+// };
--- app/api/resend/route.ts
+++ app/api/resend/route.ts
@@ -1,27 +1,27 @@
-import { EmailTemplate } from '@/app/[locale]/(teacher)/(dashboard)/settings/notifications/EmailTemplate';
-import { NextApiRequest, NextApiResponse } from 'next';
-import { Resend } from 'resend';
+// import { EmailTemplate } from '@/app/[locale]/(teacher)/(dashboard)/settings/notifications/EmailTemplate';
+// import { NextApiRequest, NextApiResponse } from 'next';
+// import { Resend } from 'resend';
 
-const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
+// const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 
-export async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const method = req.method?.toLowerCase();
-    if (method === "post")
-    {
-        try {
-        const result = await resend.emails.send({
-        from: 'lemaildejo@gmail.com',
-        to: ['jcourtoi@student.42.fr'],
-        subject: 'Test email',
-        react: EmailTemplate({ name: 'Johanna' }),
-        });
+// export async function handler(req: NextApiRequest, res: NextApiResponse) {
+//     const method = req.method?.toLowerCase();
+//     if (method === "post")
+//     {
+//         try {
+//         const result = await resend.emails.send({
+//         from: 'lemaildejo@gmail.com',
+//         to: ['jcourtoi@student.42.fr'],
+//         subject: 'Test email',
+//         react: EmailTemplate({ name: 'Johanna' }),
+//         });
 
-        console.log(result.data);
-        res.status(200).json({ message: 'Email sent successfully !', data: result.data });
-        } catch (error) {
-            console.error('Error sending email:', error);
-            res.status(500).json({ message: 'Error sending email.', error });
-        }
-    }
+//         console.log(result.data);
+//         res.status(200).json({ message: 'Email sent successfully !', data: result.data });
+//         } catch (error) {
+//             console.error('Error sending email:', error);
+//             res.status(500).json({ message: 'Error sending email.', error });
+//         }
+//     }
   
-}
+// }
