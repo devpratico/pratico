3235f7deee7cd5f534e2a3726e8681305b1bc9c3
Johanna <jcourtoi@student.42.fr>
2024-09-06 12:25:00 +0200

    UPDATE Notif discord forgotten password style

--- app/[locale]/(teacher)/(dashboard)/settings/_buttons/PasswordForgottenBtn.tsx
+++ app/[locale]/(teacher)/(dashboard)/settings/_buttons/PasswordForgottenBtn.tsx
@@ -1,5 +1,5 @@
 import React, { SetStateAction, useEffect, useState } from 'react';
-import { Button, Flex, Tabs, TextField } from '@radix-ui/themes';
+import { Button, Flex, Link, TextField } from '@radix-ui/themes';
 import { useRouter } from 'next/navigation';
 import * as Form from '@radix-ui/react-form';
 import { Mail } from 'lucide-react';
@@ -7,6 +7,7 @@ import { Mail } from 'lucide-react';
 export default function PasswordForgottenBtn({ clicked, onClick }:{ clicked: boolean, onClick: React.Dispatch<SetStateAction<boolean>>}) {
   const [open, setOpen] = useState(false);
   const [email, setEmail] = useState('');
+  const [validEmail, setValidEmail] = useState<boolean | null>();
   const [successToast, setSuccessToast] = useState({show: false, msg: ''});
   
   const router = useRouter();
@@ -14,24 +15,32 @@ export default function PasswordForgottenBtn({ clicked, onClick }:{ clicked: boo
 
   const handleClick = async (event: React.FormEvent) => {
     event.preventDefault();
+    setValidEmail(isValidEmail(email));
     onClick(true);
-    try {
-      if (timeout !== null) {
-        clearTimeout(timeout);
+    if (validEmail)
+    {
+      try {
+        if (timeout !== null) {
+          clearTimeout(timeout);
+        }
+        await sendMessage(email);
+        setSuccessToast({ show: true, msg: 'Votre problème va être résolu au plus vite !'});
+        timeout = setTimeout(() => {
+          setOpen(false);
+          setEmail('');
+          router.push('/');
+        }, 1000);
+      } catch (error) {
+        console.error('Error sending message:', error);
+        setSuccessToast({ show: true, msg: "Quelque chose s'est mal passé, merci de nous contacter"});
+        if (timeout !== null) {
+          clearTimeout(timeout);
+        }
       }
-      await sendMessage(email);
-      setSuccessToast({ show: true, msg: 'Votre problème va être résolu au plus vite !'});
-      timeout = setTimeout(() => {
-        setOpen(false);
-        setEmail('');
-        router.push('/');
-      }, 3000);
-    } catch (error) {
-      console.error('Error sending message:', error);
-      setSuccessToast({ show: true, msg: "Quelque chose s'est mal passé, merci de nous contacter"});
-      if (timeout !== null) {
-        clearTimeout(timeout);
-      }
+    }
+    else
+    {
+      setSuccessToast({show: false, msg: 'Adresse mail non valide'});
     }
   };
 
@@ -52,10 +61,24 @@ export default function PasswordForgottenBtn({ clicked, onClick }:{ clicked: boo
     console.log(data.success ? 'Message sent successfully' : 'Error while sending msg');
   };
 
+
+  function isValidEmail(email: string) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+  
+  const EmailInvalid = () => {
+    const msg = <>
+      {successToast.msg}
+    </>
+  
+    return (msg);
+  };
+
   return (
     <div>
       {!open && !clicked ? (
-        <Button onClick={() => { onClick(true); setOpen(true)}}>Mot de passe oublié ?</Button>
+        <Link onClick={() => { onClick(true); setOpen(true)}} style={{ cursor: 'pointer' }}>Mot de passe oublié ?</Link>
       ) : (
           successToast.show
           ? <>
@@ -66,23 +89,29 @@ export default function PasswordForgottenBtn({ clicked, onClick }:{ clicked: boo
 
               <Form.Field key='email' name='email'>
                   <Form.Control asChild>
-                      <TextField.Root placeholder={'email'} value={email} onChange={(e) => setEmail(e.target.value)}required>
+                      <TextField.Root placeholder={'email'}
+                        value={email}
+                        style={{color: validEmail === false ? '#CC0000' : undefined}}
+                        onChange={(e) => {
+                          setEmail(e.target.value);
+                          if (!isValidEmail(email))
+                             setValidEmail(null);
+                          else
+                            setValidEmail(true);
+                        }}
+                        required>
                           <TextField.Slot><Mail /></TextField.Slot>
                       </TextField.Root>
                   </Form.Control>
               </Form.Field>
-
-              {/* <Form.Field key='name' name='name'>
-                  <Form.Control asChild>
-                      <TextField.Root placeholder={'prenom'}>
-                          <TextField.Slot><RectangleEllipsis /></TextField.Slot>
-                      </TextField.Root>
-                  </Form.Control>
-              </Form.Field> */}
-
-              <Form.Submit asChild>
+              {
+                (validEmail === false)
+                ? <EmailInvalid />
+                : null
+              }
+              <Form.Submit asChild >
                   <Button onClick={handleClick}>
-                      Mot de passe oublié
+                      Envoyer
                   </Button>
               </Form.Submit>
 
