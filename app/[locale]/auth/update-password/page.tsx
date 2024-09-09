b727404031472b165d7d150b6e05d9b962e3c682
Johanna <jcourtoi@student.42.fr>
2024-09-09 16:15:13 +0200

    UPDATE reset password not same pw:
    The user cannot give a password he already used before

--- app/[locale]/auth/update-password/page.tsx
+++ app/[locale]/auth/update-password/page.tsx
@@ -2,13 +2,13 @@
 
 import React, { useState } from 'react';
 import * as Form from '@radix-ui/react-form';
-import { Button, TextField } from '@radix-ui/themes';
+import { Button, Flex, TextField } from '@radix-ui/themes';
 import createClient from '@/supabase/clients/client';
-// import { useRouter } from '@/app/_intl/intlNavigation';
+import { useRouter } from '@/app/_intl/intlNavigation';
 
 export default function UpdatePassword() {
   const [newPassword, setNewPassword] = useState('');
-//   const router = useRouter();
+  const router = useRouter();
   const supabase = createClient();
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -19,11 +19,15 @@ export default function UpdatePassword() {
         password: newPassword
       });
       
-      if (data) {
-        alert("Mot de passe mis à jour avec succès!");
-        // router.push('/');
-      } else if (error) {
-        alert("Une erreur s'est produite lors de la mise à jour du mot de passe.");
+      if (data.user) {
+        console.log('DATA PW:', data);
+        alert('Mot de passe mis à jour avec succès!');
+        router.push('/auth?authTab=login');
+      } else {
+        if (!data.user)
+          alert('Vous ne pouvez pas reprendre un mot de passe deja utilise.')
+        if (error)
+          alert("Une erreur s'est produite lors de la mise à jour du mot de passe.");
       }
     } catch (error) {
       console.error('Error:', error);
@@ -31,16 +35,21 @@ export default function UpdatePassword() {
   };
 
   return (
-    <Form.Root onSubmit={handleSubmit}>
-        <Form.Field name='password'>
-            <TextField.Root
-                value={newPassword}
-                onChange={(e) => setNewPassword(e.target.value)}
-                placeholder="Nouveau mot de passe">
-            </TextField.Root>
-        </Form.Field>
-        <Button type="submit">Changer le mot de passe</Button>
-  </Form.Root>
-  
+    <Flex direction='column' gap='2' pt='5'>
+ 
+      <Form.Root onSubmit={handleSubmit}>
+
+          <Form.Field name='password'>
+              <TextField.Root
+                  value={newPassword}
+                  onChange={(e) => setNewPassword(e.target.value)}
+                  placeholder="Nouveau mot de passe">
+              </TextField.Root>
+          </Form.Field>
+          <Button type="submit">Changer le mot de passe</Button>
+
+      </Form.Root>
+
+    </Flex>
   );
 }
