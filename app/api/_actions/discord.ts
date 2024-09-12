03958faa48c5055909c7c1f4df3f118e4888a3cf
Thomas DOUCHE <Thomas.douche@aol.com>
2024-09-12 17:39:28 +0200

    UPDATE discord bot now a server action

--- app/api/_actions/discord.ts
+++ app/api/_actions/discord.ts
@@ -1,16 +1,33 @@
-export const sendMessage = async (message: string) => {
-    const response = await fetch('/api/bot', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ message }),
-    });
+import logger from "@/app/_utils/logger";
 
-    if (!response.ok) {
-      throw new Error('Error sending message');
+const webhookUrl = process.env.DISCORD_WEBHOOK_URL || '';
+
+
+export const sendDiscordMessage = async (message: string) => {
+
+    if (!webhookUrl) {
+        logger.error('next:api', 'sendDiscordMessage', 'No webhook url provided. Please provide the DISCORD_WEBHOOK_URL environment variable');
+        return;
     }
 
-    const data = await response.json();
-    console.log(data.success ? 'Message sent successfully' : 'Error while sending msg');
-  };
+    try {
+        const response = await fetch(webhookUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                content: message,
+            }),
+        });
+
+        if (!response.ok) {
+            logger.error('next:api', 'sendDiscordMessage', 'Error sending message');
+        }
+
+        return;
+
+    } catch (error) {
+        logger.error('next:api', 'sendDiscordMessage', 'Error sending message:', error);
+    }
+};
