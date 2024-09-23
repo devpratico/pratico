49ddb58ec73157778f2782a061326334d9b77c99
Johanna <jcourtoi@student.42.fr>
2024-09-23 15:42:16 +0200

    ADD capsule title

--- app/[locale]/(teacher)/(dashboard)/reports/[capsule_id]/page.tsx
+++ app/[locale]/(teacher)/(dashboard)/reports/[capsule_id]/page.tsx
@@ -5,8 +5,9 @@ import logger from '@/app/_utils/logger';
 import { useEffect, useState } from 'react';
 import { fetchRoomsByCapsuleId } from '@/app/api/_actions/room';
 import { fetchAttendanceByRoomId } from '@/app/api/_actions/attendance';
-import { Callout, Container, Grid, Heading, Link, ScrollArea, Section } from '@radix-ui/themes';
+import { Callout, Container, Grid, Heading, Link, ScrollArea, Section, Text } from '@radix-ui/themes';
 import { formatDate, sanitizeUuid } from '@/app/_utils/utils_functions';
+import { fetchCapsule } from '@/app/api/_actions/capsule';
 
 // TYPE
 export type SessionInfoType = {
@@ -17,61 +18,66 @@ export type SessionInfoType = {
 };
 
 export default function CapsuleSessionReportPage() {
-  const searchParams = useSearchParams().getPathnameWithoutSearchParam('capsuleId');
-  const router = useRouter();
-  const capsuleId = searchParams.split('/').pop();
-  const [sessionInfo, setSessionInfo] = useState<SessionInfoType[] | null>(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-
-  if (!capsuleId) {
-    logger.error('next:page', 'ReportsOfCapsulePage', 'capsuleId missing');
-    router.push('/reports');
-    return;
-  }
+	const searchParams = useSearchParams().getPathnameWithoutSearchParam('capsuleId');
+	const router = useRouter();
+	const capsuleId = searchParams.split('/').pop();
+	const [sessionInfo, setSessionInfo] = useState<SessionInfoType[] | null>(null);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState<string | null>(null);
+	const [ capsuleTitle, setCapsuleTitle ] = useState("");
 
   useEffect(() => {
     const getSessions = async () => {
-      setLoading(true);
-      try {
-        let sessions: SessionInfoType[] = [];
-        const { data: roomData, error: roomError } = await fetchRoomsByCapsuleId(capsuleId);
-        logger.debug('supabase:database', 'CapsuleSessionsReportServer', 'fetchRoomsByCapsuleId datas', roomData, roomError);
+		setLoading(true);
+		try {
+			if (!capsuleId)
+				return ;
+			let sessions: SessionInfoType[] = [];		
+			const { data: roomData, error: roomError } = await fetchRoomsByCapsuleId(capsuleId);
+			logger.debug('supabase:database', 'CapsuleSessionsReportServer', 'fetchRoomsByCapsuleId datas', roomData, roomError);
+			if (!roomData || roomError) {
+			logger.error('supabase:database', 'CapsuleSessionsReportServer', roomError ? roomError : 'No rooms data for this capsule');
+			setError(roomError ? 'Une erreur est survenue lors de la récupération des sessions.' : 'Aucune sessions');
+			return;
+			}
+			const { data: capsuleData, error: capsuleError } = await fetchCapsule(capsuleId);
+			if (capsuleData)
+				setCapsuleTitle(capsuleData?.title ? capsuleData.title : "");
+			console.log("JIJDIJI", capsuleTitle);
+			await Promise.all(
+				roomData.map(async (room) => {
+					const { data, error } = await fetchAttendanceByRoomId(room.id);
+					if (!data || error) {
+					logger.error('supabase:database', 'CapsuleSessionsReportServer', error ? error : 'No attendance data for this room');
+					}
+					const infos: SessionInfoType = {
+						id: room.id.toString(),
+						created_at: room.created_at,
+						numberOfParticipant: data ? data.length : 0,
+						status: room.status,
+					};
+					sessions.push(infos);
+				})
+			);
 
-        if (!roomData || roomError) {
-          logger.error('supabase:database', 'CapsuleSessionsReportServer', roomError ? roomError : 'No rooms data for this capsule');
-          setError(roomError ? 'Une erreur est survenue lors de la récupération des sessions.' : 'Aucune sessions');
-          return;
-        }
-
-        await Promise.all(
-          roomData.map(async (room) => {
-            const { data, error } = await fetchAttendanceByRoomId(room.id);
-            if (!data || error) {
-              logger.error('supabase:database', 'CapsuleSessionsReportServer', error ? error : 'No attendance data for this room');
-            }
-
-            const infos: SessionInfoType = {
-              id: room.id.toString(),
-              created_at: room.created_at,
-              numberOfParticipant: data ? data.length : 0,
-              status: room.status,
-            };
-            sessions.push(infos);
-          })
-        );
-
-        setSessionInfo(sessions);
-      } catch (err) {
-        console.error('Error getting sessions', err);
-        setError('Erreur lors de la récupération des sessions.');
-      } finally {
-        setLoading(false);
-      }
+			setSessionInfo(sessions);
+		} catch (err) {
+			console.error('Error getting sessions', err);
+			setError('Erreur lors de la récupération des sessions.');
+		} finally {
+			setLoading(false);
+		}
     };
 
     getSessions();
   }, [capsuleId]);
+
+  if (!capsuleId) {
+    logger.error('next:page', 'ReportsOfCapsulePage', 'capsuleId missing');
+    router.push('/reports');
+	return ;
+  }
+
 
   logger.log('next:page', 'Reports of capule #', capsuleId);
 
@@ -79,7 +85,7 @@ export default function CapsuleSessionReportPage() {
     <ScrollArea>
       <Section px={{ initial: '3', xs: '0' }}>
         <Container>
-          <Heading as='h1'>Rapports</Heading>
+			<Heading as="h1">{capsuleTitle.length ? capsuleTitle : "Capsule sans titre"}</Heading>
           <Callout.Root mt='4'>
             <Grid columns='repeat(auto-fill, minmax(200px, 1fr))' gap='3'>
 				{
