28d2f121178db1743a28e06d1368b88c8bde8388
Johanna <jcourtoi@student.42.fr>
2024-09-25 10:52:44 +0200

    ADD second part of listing reports, not working yet

--- app/[locale]/(teacher)/(dashboard)/reports/page.tsx
+++ app/[locale]/(teacher)/(dashboard)/reports/page.tsx
@@ -1,9 +1,14 @@
-import { Container, Section, Heading, Callout, Grid, Box, Link, ScrollArea } from '@radix-ui/themes';
+import { Container, Section, Heading, Callout, Grid, Box, Link as RadixLink, ScrollArea } from '@radix-ui/themes';
 import CapsuleReports from './_components/CapsuleReports';
 import { fetchUser } from '@/app/api/_actions/user';
 import { fetchCapsulesData } from '@/app/api/_actions/capsule';
 import { Json } from '@/supabase/types/database.types';
 import logger from '@/app/_utils/logger';
+import Link from 'next/link';
+import { fetchRoomsByCapsuleId } from '@/app/api/_actions/room';
+import { useRouter } from 'next/navigation';
+import { fetchAttendancesByRoomId } from '@/app/api/_actions/attendance';
+import { SessionInfoType } from './[capsule_id]/page';
 
 // TYPE
 export type CapsuleType = {
