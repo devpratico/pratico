a667d9787c28e3286064dd20c037c338e9af1b69
Johanna <jcourtoi@student.42.fr>
2024-10-03 16:59:58 +0200

    UPDATE generate pdf works

--- app/[locale]/(teacher)/(dashboard)/reports/_components/AttendanceToPDF.tsx
+++ app/[locale]/(teacher)/(dashboard)/reports/_components/AttendanceToPDF.tsx
@@ -1,22 +1,15 @@
 "use client";
-import { Button, Flex, Heading, Link, Table } from "@radix-ui/themes";
-import { TableCell } from "./TableCell";
+import { Button, Flex, Heading, Link, Text } from "@radix-ui/themes";
 import { AttendanceInfoType } from "../[room_id]/page";
 import { ArrowLeft } from "lucide-react";
-import { useEffect, useRef } from "react";
-import generatePDF from "react-to-pdf";
+import { usePDF } from "react-to-pdf";
+import { janifera } from "@/app/Fonts";
 
 export default function AttendanceToPDF ({ attendances, sessionDate, capsuleTitle, roomId }:
 	{ attendances: AttendanceInfoType[], sessionDate: string | undefined, capsuleTitle: string, roomId: string }
 ) {
-	const targetRef = useRef<HTMLDivElement>(null);
-	useEffect(() => {
-		if (targetRef.current) {
-		  targetRef.current.style.color = 'black';
-		  targetRef.current.style.backgroundColor = 'white';
-		  targetRef.current.style.padding = '10px';
-		}
-	  }, []);
+	const { toPDF, targetRef } = usePDF({filename: "emargement.pdf"});
+
 	return (
 		<>
 			<Flex justify='between'>
@@ -25,39 +18,57 @@ export default function AttendanceToPDF ({ attendances, sessionDate, capsuleTitl
 						<ArrowLeft />Retour
 					</Link>
 				</Button>
-				<Button onClick={() => generatePDF(targetRef , {filename: 'emargement.pdf'})}>Générer PDF</Button>
+				<Button onClick={() => toPDF()}>Générer PDF</Button>
 			</Flex>
 
 			<div ref={targetRef} style={{color: '#000000' , backgroundColor: '#FFFFFF', padding: '10px'}} >
 				<Heading mb="4" as="h2">{`${capsuleTitle !== "Sans titre" ? capsuleTitle : ""}`}</Heading>
 				<Heading ml="3" mb="4" as="h4">{`Emargements${sessionDate ? ` du ${sessionDate}` : ""}`}</Heading>
 
-				<Table.Root variant="surface">
-					
-					<Table.Header>
-						<Table.Row>
-							<Table.ColumnHeaderCell>Prénom</Table.ColumnHeaderCell>
-							<Table.ColumnHeaderCell>Nom</Table.ColumnHeaderCell>
-							<Table.ColumnHeaderCell>{"Heure d'arrivée"}</Table.ColumnHeaderCell>
-							<Table.ColumnHeaderCell>Signature</Table.ColumnHeaderCell>
-						</Table.Row>
-					</Table.Header>
-					<Table.Body>
-						{
-							!attendances.length
-							? <Table.Row>
-								<Table.Cell>
-									Aucun participant
-								</Table.Cell>
-							</Table.Row>
-							: attendances?.map((attendance, index) => {
-								return (
-									<TableCell key={index} navigationsIds={{roomId}} infos={{roomClosed: true, rowHeaderCell: attendance.first_name, cellOne: attendance.last_name, cellTwo: attendance.connexion}} />
-								);
-							})
-						}
-					</Table.Body>
-				</Table.Root>
+				<table style={{ width: '100%', borderCollapse: 'collapse' }}>
+					<thead style={{ backgroundColor: '#f0f0f0', borderBottom: '2px solid #ddd' }}>
+						<tr>
+						<th style={{ padding: '8px', textAlign: 'left', borderBottom: '1px solid #ddd' }}>
+							<Text>Prénom</Text>
+						</th>
+						<th style={{ padding: '8px', textAlign: 'left', borderBottom: '1px solid #ddd' }}>
+							<Text>Nom</Text>
+						</th>
+						<th style={{ padding: '8px', textAlign: 'left', borderBottom: '1px solid #ddd' }}>
+							<Text>{"Heure d'arrivée"}</Text>
+						</th>
+						<th style={{ padding: '8px', textAlign: 'left', borderBottom: '1px solid #ddd' }}>
+							<Text>Signature</Text>
+						</th>
+						</tr>
+					</thead>
+					<tbody>
+						{!attendances.length ? (
+						<tr>
+							<td colSpan={4} style={{ padding: '8px', textAlign: 'center', borderBottom: '1px solid #ddd' }}>
+								<Text>Aucun participant</Text>
+							</td>
+						</tr>
+						) : (
+							attendances.map((attendance, index) => (
+								<tr key={index} style={{ borderBottom: '1px solid #ddd' }}>
+									<td style={{ padding: '8px' }}>
+										<Text>{attendance.first_name}</Text>
+									</td>
+									<td style={{ padding: '8px' }}>
+										<Text>{attendance.last_name}</Text>
+									</td>
+									<td style={{ padding: '8px' }}>
+										<Text>{attendance.connexion}</Text>
+									</td>
+									<td style={{ padding: '8px' }}>
+										<Text className={janifera.className}>{`${attendance.first_name} ${attendance.last_name}`}</Text>
+									</td>
+								</tr>
+							))
+						)}
+					</tbody>
+				</table>
 			</div>
 		</>		
 	);
