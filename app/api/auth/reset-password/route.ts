a6a6f1b29d1c809bc0e69ac0ddd6bbe3e0437b67
Thomas DOUCHE <Thomas.douche@aol.com>
2024-10-07 14:22:58 +0200

    use POST instead of GET

--- app/api/auth/reset-password/route.ts
+++ app/api/auth/reset-password/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server"
 import createClient from '@/supabase/clients/server'
 
 
-export async function GET(request: NextRequest) {
+export async function POST(request: NextRequest) {
     const { searchParams } = new URL(request.url)
     const email = searchParams.get('email')
 
