6e396528a561411adc131a21f517f5137cf64700
Thomas DOUCHE <Thomas.douche@aol.com>
2024-10-07 23:32:08 +0200

    remove recursive call to Discord api

--- app/api/discord/log-error/route.ts
+++ app/api/discord/log-error/route.ts
@@ -1,34 +1,30 @@
-import logger from '@/app/_utils/logger';
 import { NextRequest, NextResponse } from 'next/server';
 import createClient from '@/supabase/clients/server';
-import { use } from 'react';
+
 
 const webhookUrl = process.env.DISCORD_WEBHOOK_URL || '';
 
 
 export async function POST(request: NextRequest) {
     if (!webhookUrl) {
-        logger.error('next:api', 'sendDiscordMessage', 'No webhook url provided. Please provide the DISCORD_WEBHOOK_URL environment variable');
+        console.error('next:api', 'sendDiscordMessage', 'No webhook url provided. Please provide the DISCORD_WEBHOOK_URL environment variable');
         return NextResponse.json({ error: 'No webhook url provided' }, { status: 400 });
     }
 
     const { message: messageContent } = await request.json();
 
     if (!messageContent) {
-        logger.error('next:api', 'sendDiscordMessage', 'No message provided');
+        console.error('next:api', 'sendDiscordMessage', 'No message provided');
         return NextResponse.json({ error: 'No message provided' }, { status: 400 });
     }
 
     const supabase = createClient()
     const {data: userData, error: userError } = await supabase.auth.getUser()
 
-    const userId = userData?.user?.id
-    const email = userData?.user?.email
-
     let names: string | undefined
 
-    if (userId) {
-        const { data: _names, error: namesError } = await supabase.from('user_profiles').select('first_name, last_name').eq('id', userId).single()
+    if (!userError && userData?.user?.id) {
+        const { data: _names, error: namesError } = await supabase.from('user_profiles').select('first_name, last_name').eq('id', userData.user.id).single()
         if (!namesError && _names.first_name && _names.first_name !== '' && _names.last_name && _names.last_name !== '') {
             names = `${_names.first_name} ${_names.last_name}`
         }
@@ -55,14 +51,14 @@ export async function POST(request: NextRequest) {
         });
 
         if (!response.ok) {
-            logger.error('next:api', 'sendDiscordMessage', 'Error sending message');
+            console.error('next:api', 'sendDiscordMessage', 'Error sending message');
             return NextResponse.json({ error: 'Error sending message' }, { status: 500 });
         }
 
         return NextResponse.json({ msg: 'Message sent' }, { status: 200 });
 
     } catch (error) {
-        logger.error('next:api', 'sendDiscordMessage', 'Error sending message:', error);
+        console.error('next:api', 'sendDiscordMessage', 'Error sending message:', error);
         return NextResponse.json({ error: 'Error sending message' }, { status: 500 });
     }
 }
--- app/api/discord/send-message/route.ts
+++ app/api/discord/send-message/route.ts
@@ -1,4 +1,3 @@
-import logger from '@/app/_utils/logger';
 import { NextRequest, NextResponse } from 'next/server';
 
 const webhookUrl = process.env.DISCORD_WEBHOOK_URL || '';
@@ -6,14 +5,14 @@ const webhookUrl = process.env.DISCORD_WEBHOOK_URL || '';
 
 export async function POST(request: NextRequest) {
     if (!webhookUrl) {
-        logger.error('next:api', 'sendDiscordMessage', 'No webhook url provided. Please provide the DISCORD_WEBHOOK_URL environment variable');
+        console.error('next:api', 'sendDiscordMessage', 'No webhook url provided. Please provide the DISCORD_WEBHOOK_URL environment variable');
         return NextResponse.json({ error: 'No webhook url provided' }, { status: 400 });
     }
 
     const { message } = await request.json();
 
     if (!message) {
-        logger.error('next:api', 'sendDiscordMessage', 'No message provided');
+        console.error('next:api', 'sendDiscordMessage', 'No message provided');
         return NextResponse.json({ error: 'No message provided' }, { status: 400 });
     }
 
@@ -25,14 +24,14 @@ export async function POST(request: NextRequest) {
         });
 
         if (!response.ok) {
-            logger.error('next:api', 'sendDiscordMessage', 'Error sending message');
+            console.error('next:api', 'sendDiscordMessage', 'Error sending message');
             return NextResponse.json({ error: 'Error sending message' }, { status: 500 });
         }
 
         return NextResponse.json({ msg: 'Message sent' }, { status: 200 });
 
     } catch (error) {
-        logger.error('next:api', 'sendDiscordMessage', 'Error sending message:', error);
+        console.error('next:api', 'sendDiscordMessage', 'Error sending message:', error);
         return NextResponse.json({ error: 'Error sending message' }, { status: 500 });
     }
 }
