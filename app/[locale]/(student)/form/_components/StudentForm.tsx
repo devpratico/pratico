5dee134cf029ff6d04f0738dc419c18c32fec03f
Johanna <jcourtoi@student.42.fr>
2024-10-07 15:08:36 +0200

    FIX maxSize formulaire

--- app/[locale]/(student)/form/_components/StudentForm.tsx
+++ app/[locale]/(student)/form/_components/StudentForm.tsx
@@ -60,7 +60,6 @@ export default function StudentForm() {
                 <Form.Field key='first-name' name='first-name'>
                     <Form.Control asChild>
                         <TextField.Root onChange={(e) => {
-							if (e.target.value.length < 100)
 								setName({...name, firstname: e.target.value})
 							}}
 							placeholder='Prénom' required/>
@@ -70,8 +69,7 @@ export default function StudentForm() {
                 <Form.Field key='last-name' name='last-name'>
                     <Form.Control asChild>
                         <TextField.Root onChange={(e) =>  {
-								if (e.target.value.length < 100)
-									setName({...name, lastname: e.target.value})
+								setName({...name, lastname: e.target.value})
 							}}
 							placeholder='Nom' required/>
                     </Form.Control>
@@ -86,8 +84,8 @@ export default function StudentForm() {
 					</Flex>
 				</Box>
 
-				<Flex mt='5' justify='between' height='20px'>
-					<Text mr='5' size='8' className={janifera.className}>{`${name.firstname} ${name.lastname}`}</Text>
+				<Flex mt='5' justify='between' height='30px'>
+					<Text style={{maxInlineSize: '1200px'}} mr='5' size='8' className={janifera.className}>{`${name.firstname} ${name.lastname}`}</Text>
 
 					<Button onClick={() => setChecked({...checked, submit: true})} type="submit" loading={isLoading}>OK</Button>
 				</Flex>
