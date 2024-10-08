08f4ade7e8399a3b3ae57961ee3e2ff0bb6587d7
Johanna <jcourtoi@student.42.fr>
2024-10-08 15:15:05 +0200

    FIX (try) color issue on chrome with not radix colors

--- app/[locale]/(teacher)/(dashboard)/reports/_components/AttendanceToPDF.tsx
+++ app/[locale]/(teacher)/(dashboard)/reports/_components/AttendanceToPDF.tsx
@@ -48,70 +48,70 @@ export default function AttendanceToPDF ({ attendances, sessionDate, capsuleTitl
 			</Flex>
 
 			<div ref={targetRef}>
-			<div style={{margin: "100px"}} >
-				<p style={{paddingTop: '50px'}}>Pratico</p>
-				<h1 style={{textAlign: 'center', margin: "50px "}}>Rapport de Session</h1>
-				<h2>{`${capsuleTitle !== "Sans titre" ? capsuleTitle : ""}`}</h2>
-				<h3 style={{color: "var(--gray-8)"}}>{`${date ? `Session du ${date}` : ""} ${date && hour ? ` à ${hour}` : ""}`}</h3>
-				{
-					user ?
-					<>
-						<h4>{`Animateur: ${user?.first_name} ${user?.last_name}`}</h4>
-						<h1 style={{ marginLeft: "20px", marginBottom: "50px"}} className={janifera.className}>{`${user.first_name} ${user.last_name}`}</h1>
-					</>
+				<div style={{margin: "100px"}} >
+					<p style={{paddingTop: '50px'}}>Pratico</p>
+					<h1 style={{textAlign: 'center', margin: "50px "}}>Rapport de Session</h1>
+					<h2>{`${capsuleTitle !== "Sans titre" ? capsuleTitle : ""}`}</h2>
+					<h3 style={{color: "#666666"}}>{`${date ? `Session du ${date}` : ""} ${date && hour ? ` à ${hour}` : ""}`}</h3>
+					{
+						user ?
+						<>
+							<h4>{`Animateur: ${user?.first_name} ${user?.last_name}`}</h4>
+							<h1 style={{ marginLeft: "20px", marginBottom: "50px"}} className={janifera.className}>{`${user.first_name} ${user.last_name}`}</h1>
+						</>
 
-					: ""
-				}
-				<Flex justify="between" mb='3'>
-					<h2>Émargement</h2>
-					<Text>{`Participants: ${attendances.length || "Aucun"}`}</Text>		
-				</Flex>
+						: ""
+					}
+					<Flex justify="between" mb='3'>
+						<h2>Émargement</h2>
+						<Text>{`Participants: ${attendances.length || "Aucun"}`}</Text>		
+					</Flex>
 
-				<table style={{ width: '100%', borderCollapse: 'collapse', border: '1px solid var(--gray-6)'}}>
-					<thead style={{ backgroundColor: 'var(--gray-3)', borderBottom: '2px solid var(--gray-6)' }}>
-						<tr>
-						<th style={{  maxInlineSize: '200px', padding: '10px', textAlign: 'left', borderBottom: '1px solid var(--gray-3)' }}>
-							<Text ml='1'>Prénom</Text>
-						</th>
-						<th style={{ maxInlineSize: '200px', padding: '10px', textAlign: 'left', borderBottom: '1px solid var(--gray-3)' }}>
-							<Text ml='1'>Nom</Text>
-						</th>
-						<th style={{ maxInlineSize: '100px', padding: '10px', textAlign: 'left', borderBottom: '1px solid var(--gray-3)' }}>
-							<Text ml='1'>{"Heure d'arrivée"}</Text>
-						</th>
-						<th style={{ maxInlineSize: '200px', padding: '10px', textAlign: 'left', borderBottom: '1px solid var(--gray-3)' }}>
-							<Text ml='1'>Signature</Text>
-						</th>
-						</tr>
-					</thead>
-					<tbody style={{backgroundColor: 'white'}}>
-						{
-							!sortedAttendances || !sortedAttendances.length ?
+					<table style={{ width: '100%', borderCollapse: 'collapse', border: '1px solid #cccccc'}}>
+						<thead style={{ backgroundColor: ' #f2f2f2', borderBottom: '2px solid #cccccc' }}>
 							<tr>
-								<td colSpan={4} style={{ padding: '10px', textAlign: 'center', borderBottom: '1px solid var(--gray-3)' }}>
-									<Text>Aucun participant</Text>
-								</td>
+							<th style={{  maxInlineSize: '200px', padding: '10px', textAlign: 'left', borderBottom: '1px solid  #f2f2f2' }}>
+								<Text ml='1'>Prénom</Text>
+							</th>
+							<th style={{ maxInlineSize: '200px', padding: '10px', textAlign: 'left', borderBottom: '1px solid  #f2f2f2' }}>
+								<Text ml='1'>Nom</Text>
+							</th>
+							<th style={{ maxInlineSize: '100px', padding: '10px', textAlign: 'left', borderBottom: '1px solid  #f2f2f2' }}>
+								<Text ml='1'>{"Heure d'arrivée"}</Text>
+							</th>
+							<th style={{ maxInlineSize: '200px', padding: '10px', textAlign: 'left', borderBottom: '1px solid  #f2f2f2' }}>
+								<Text ml='1'>Signature</Text>
+							</th>
 							</tr>
-							: sortedAttendances.map((attendance, index) => (
-								<tr key={index} style={{ borderBottom: '1px solid var(--gray-3)' }}>
-									<td style={{ maxInlineSize: '200px', padding: '10px' }}>
-										<Text ml='1'>{attendance.first_name}</Text>
-									</td>
-									<td style={{ maxInlineSize: '200px', padding: '10px' }}>
-										<Text ml='1'>{attendance.last_name}</Text>
-									</td>
-									<td style={{ maxInlineSize: '100px', padding: '10px' }}>
-										<Text ml='1'>{attendance.connexion}</Text>
-									</td>
-									<td style={{ maxInlineSize: '200px', padding: '10px' }}>
-										<Text ml='1' className={janifera.className}>{`${attendance.first_name} ${attendance.last_name}`}</Text>
+						</thead>
+						<tbody style={{backgroundColor: 'white'}}>
+							{
+								!sortedAttendances || !sortedAttendances.length ?
+								<tr>
+									<td colSpan={4} style={{ padding: '10px', textAlign: 'center', borderBottom: '1px solid #f2f2f2' }}>
+										<Text>Aucun participant</Text>
 									</td>
 								</tr>
-							)
-						)}
-					</tbody>
-				</table>
-			</div>
+								: sortedAttendances.map((attendance, index) => (
+									<tr key={index} style={{ borderBottom: '1px solid  #f2f2f2' }}>
+										<td style={{ maxInlineSize: '200px', padding: '10px' }}>
+											<Text ml='1'>{attendance.first_name}</Text>
+										</td>
+										<td style={{ maxInlineSize: '200px', padding: '10px' }}>
+											<Text ml='1'>{attendance.last_name}</Text>
+										</td>
+										<td style={{ maxInlineSize: '100px', padding: '10px' }}>
+											<Text ml='1'>{attendance.connexion}</Text>
+										</td>
+										<td style={{ maxInlineSize: '200px', padding: '10px' }}>
+											<Text ml='1' className={janifera.className}>{`${attendance.first_name} ${attendance.last_name}`}</Text>
+										</td>
+									</tr>
+								)
+							)}
+						</tbody>
+					</table>
+				</div>
 			</div>
 		</>		
 	);
