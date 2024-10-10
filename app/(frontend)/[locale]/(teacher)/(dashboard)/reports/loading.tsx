6fc0fa7612a28d832df91ea8c60061367ae0829c
Johanna <jcourtoi@student.42.fr>
2024-10-10 14:36:36 +0200

    UPDATE loading for reports

--- app/(frontend)/[locale]/(teacher)/(dashboard)/reports/loading.tsx
+++ app/(frontend)/[locale]/(teacher)/(dashboard)/reports/loading.tsx
@@ -1,23 +1,12 @@
-import { Flex, Spinner, Text } from "@radix-ui/themes";
+import { Container, Section, Spinner } from "@radix-ui/themes";
 
 export default function Loading () {
 
 	return (
-		<Text as="div" style={{
-			position: 'fixed',
-			top: 0,
-			left: 0,
-			width: '100%',
-			height: '100%',
-			backgroundColor: 'rgba(255, 255, 255, 0.7)',
-			display: 'flex',
-			justifyContent: 'center',
-			alignItems: 'center',
-			zIndex: 9999
-		  }}>
-			<Flex>
-				<Spinner mr='2'/> Chargement...
-			</Flex>
-		  </Text>
+		<Section>
+			<Container>
+				<Spinner mr='2'/>
+			</Container>
+		</Section>
 	)
 };
