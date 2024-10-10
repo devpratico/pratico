6ffe405b0a89097c724b9e5b83d46f79bb40ffd7
Johanna <jcourtoi@student.42.fr>
2024-10-10 10:03:03 +0200

    UPDATE marge top and sides

--- app/(frontend)/[locale]/(teacher)/(dashboard)/reports/_components/AttendanceToPDF.tsx
+++ app/(frontend)/[locale]/(teacher)/(dashboard)/reports/_components/AttendanceToPDF.tsx
@@ -1,12 +1,11 @@
 "use client";
-import { Button, Flex, Heading, Link, Table, Text } from "@radix-ui/themes";
+import { Button, Flex, Link, Text } from "@radix-ui/themes";
 import { AttendanceInfoType } from "../[room_id]/page";
 import { ArrowLeft } from "lucide-react";
 import { janifera, luciole } from "@/app/Fonts";
 import { formatDate } from "@/app/_utils/utils_functions";
 import { useEffect, useRef, useState } from "react";
 import { useReactToPrint } from "react-to-print";
-import { TableCell } from "./TableCell";
 /// TYPE
 export type TeacherInfo = {
 	first_name: string,
@@ -72,65 +71,7 @@ export default function AttendanceToPDF ({ attendances, sessionDate, capsuleTitl
 				</Button>
 			</Flex>
 		
-			<div style={{fontSize: '12px', margin: '20px 114px'}} ref={contentRef} className={luciole.className} >
-				{/* <Text>Pratico</Text>
-				<Heading as='h1' style={{textAlign: 'center', margin: "50px "}}>Rapport de Session</Heading>
-				<Heading mb="4" as="h2">{`${capsuleTitle !== "Sans titre" ? capsuleTitle : ""}`}</Heading>
-					<Heading style={{color: "var(--gray-8)"}} ml="3" mb="4" as="h4">{`${date ? `Session du ${date}` : ""} ${date && hour ? ` à ${hour}` : ""}`}</Heading>
-					{
-						userInfo ?
-						<>
-							<Heading as="h4">{`Animateur: ${userInfo?.first_name} ${userInfo?.last_name}`}</Heading>
-							<Heading as="h1" style={{ marginLeft: "20px", marginBottom: "50px"}} className={janifera.className}>{`${userInfo.first_name} ${userInfo.last_name}`}</Heading>
-						</>
-
-						: ""
-					}
-					<Flex mt='9' mb='3' justify='between'>
-						<Heading as="h4">Émargement</Heading>
-						<Text> {`Participants: ${attendances.length || "Aucun"}`}</Text>		
-					</Flex>
-
-					<Table.Root variant="surface">
-						
-						<Table.Header>
-							<Table.Row style={{ maxInlineSize: '100px', padding: '10px' }}>
-								<Table.ColumnHeaderCell>Prénom</Table.ColumnHeaderCell>
-								<Table.ColumnHeaderCell>Nom</Table.ColumnHeaderCell>
-								<Table.ColumnHeaderCell  maxWidth={'100px'}>{"Heure d'arrivée"}</Table.ColumnHeaderCell>
-								<Table.ColumnHeaderCell>Signature</Table.ColumnHeaderCell>
-							</Table.Row>
-						</Table.Header>
-						<Table.Body>
-							{
-								!attendances.length
-								? <Table.Row>
-									<Table.Cell>
-										Aucun participant
-									</Table.Cell>
-								</Table.Row>
-								: attendances?.map((attendance, index) => {
-									return (
-										<Table.Row style={{ maxInlineSize: '100px', padding: '10px' }} key={index}>
-											<Table.Cell maxWidth={'300px'} style={{wordBreak: 'break-word', overflowWrap: 'break-word'}}>
-												{attendance.first_name}
-											</Table.Cell>
-											<Table.Cell  maxWidth={'300px'} style={{wordBreak: 'break-word', overflowWrap: 'break-word'}}>
-												{attendance.last_name}
-											</Table.Cell>
-											<Table.Cell  maxWidth={'100px'}>
-												{attendance.connexion}
-											</Table.Cell>
-											<Table.Cell  maxWidth={'300px'} className={janifera.className}>
-												{`${attendance.first_name} ${attendance.last_name}`}
-											</Table.Cell>
-										</Table.Row>
-											// <TableCell key={index} navigationsIds={{attendanceView: true, roomId}} infos={{roomClosed: true, rowHeaderCell: attendance.first_name, cellOne: attendance.last_name, cellTwo: attendance.connexion}} />
-										);
-									})
-								}
-						</Table.Body>
-					</Table.Root> */}
+			<div style={{fontSize: '12px', margin: '20px'}} ref={contentRef} className={luciole.className} >
 				<p>Pratico</p>
 				<h1 style={{ fontSize: '18px', textAlign: 'center', margin: "50px "}}>Rapport de Session</h1>
 				<h2 style={{ fontSize: '14px'}}>{`${capsuleTitle !== "Sans titre" ? capsuleTitle : ""}`}</h2>
@@ -154,18 +95,18 @@ export default function AttendanceToPDF ({ attendances, sessionDate, capsuleTitl
 					<thead style={{ backgroundColor: 'var(--gray-6)', borderBottom: '2px solid #cccccc' }}>
 					<thead style={{ backgroundColor: 'var(--gray-6)', borderBottom: '2px solid #cccccc' }}>
 						<tr>
-						<th style={{ maxInlineSize: '200px', padding: '10px', textAlign: 'left', borderBottom: '1px solid  var(--gray-3)'}}>
-							<Text ml='1'>Prénom</Text>
-						</th>
-						<th style={{ maxInlineSize: '200px', padding: '10px', textAlign: 'left', borderBottom: '1px solid  var(--gray-3)'}}>
-							<Text ml='1'>Nom</Text>
-						</th>
-						<th style={{ maxInlineSize: '100px', padding: '10px', textAlign: 'left', borderBottom: '1px solid  var(--gray-3)' }}>
-							<Text  ml='1'>{"Heure d'arrivée"}</Text>
-						</th>
-						<th style={{ maxInlineSize: '200px', padding: '10px', textAlign: 'left', borderBottom: '1px solid  var(--gray-3)' }}>
-							<Text ml='1'>Signature</Text>
-						</th>
+							<th style={{ maxInlineSize: '200px', padding: '10px', textAlign: 'left', borderBottom: '1px solid  var(--gray-3)'}}>
+								<Text ml='1'>Prénom</Text>
+							</th>
+							<th style={{ maxInlineSize: '200px', padding: '10px', textAlign: 'left', borderBottom: '1px solid  var(--gray-3)'}}>
+								<Text ml='1'>Nom</Text>
+							</th>
+							<th style={{ maxInlineSize: '100px', padding: '10px', textAlign: 'left', borderBottom: '1px solid  var(--gray-3)' }}>
+								<Text  ml='1'>{"Heure d'arrivée"}</Text>
+							</th>
+							<th style={{ maxInlineSize: '200px', padding: '10px', textAlign: 'left', borderBottom: '1px solid  var(--gray-3)' }}>
+								<Text ml='1'>Signature</Text>
+							</th>
 						</tr>
 					</thead>
 				
@@ -179,7 +120,7 @@ export default function AttendanceToPDF ({ attendances, sessionDate, capsuleTitl
 								</td>
 							</tr>
 							: sortedAttendances.map((attendance, index) => (
-								<tr className={index === 0 ? "test" : "none"} key={index} style={{ borderBottom: '1px solid  var(--gray-3)' }}>
+								<tr className={index === 0 ? "test" : "none"} key={index} style={{ pageBreakInside: 'avoid', borderBottom: '1px solid  var(--gray-3)' }}>
 									<td style={{ maxInlineSize: '200px', padding: '10px', wordBreak: 'break-word', overflowWrap: 'break-word' }}>
 										<Text ml='1'>{attendance.first_name}</Text>
 									</td>
