86fcac8713c78afabc6afe610da51f8ae8dcbbf3
Johanna <jcourtoi@student.42.fr>
2024-10-14 12:26:49 +0200

    ADD organization on PDF

--- app/(frontend)/[locale]/(teacher)/(dashboard)/reports/_components/AttendanceToPDF.tsx
+++ app/(frontend)/[locale]/(teacher)/(dashboard)/reports/_components/AttendanceToPDF.tsx
@@ -10,6 +10,10 @@ import { janifera, luciole } from "@/app/(frontend)/Fonts";
 export type TeacherInfo = {
 	first_name: string | null,
 	last_name: string | null,
+	organization?: {
+		name: string | undefined,
+		address: string | undefined
+	}
 }
 export default function AttendanceToPDF ({ attendances, sessionDate, capsuleTitle, user: { userInfo, roomId} }:
 	{ attendances: AttendanceInfoType[], sessionDate: string | undefined, capsuleTitle: string, user: { userInfo: TeacherInfo | null, roomId?: string}}
@@ -51,14 +55,26 @@ export default function AttendanceToPDF ({ attendances, sessionDate, capsuleTitl
 					<p>Pratico</p>
 					<h2 style={{ fontSize: '18px', textAlign: 'center', margin: "50px "}}>Rapport de Session</h2>
 					<h2 style={{ fontSize: '14px'}}>{`${capsuleTitle !== "Sans titre" ? capsuleTitle : ""}`}</h2>
-					<Text as='div' style={{ color: "var(--gray-8)"}}>{`${date ? `Session du ${date}` : ""} ${date && hour ? ` à ${hour}` : ""}`}</Text>
+					{
+						date !== "Invalid Date" && hour !== "Invalid Date"
+						? <Text as='div' mb='4'>{`${date ? `Session du ${date}` : ""} ${date && hour ? ` à ${hour}` : ""}`}</Text>
+						: <Text as='div' mb='4'></Text>
+					}
 					{
 						userInfo ?
 						<>
-							<Text as='div' style={{ fontSize: '14px'}}>{`Animateur: ${userInfo?.first_name} ${userInfo?.last_name}`}</Text>
+						{
+								userInfo.organization && userInfo.organization.name && userInfo.organization.address
+								? <Text >{`${userInfo.organization.name}, ${userInfo.organization.address}`}</Text>
+								: <></>
+							}
+							<Text as='div'>
+								<Text style={{ fontSize: '12px', fontWeight: 'bold'}}>Animateur: </Text>
+								<Text >{`${userInfo.first_name}, ${userInfo.last_name}`}</Text>
+							</Text>	
+							
 							<Text as='div' style={{ fontSize: '25px', marginLeft: "20px" }} className={janifera.className}>{`${userInfo.first_name} ${userInfo.last_name}`}</Text>
 						</>
-
 						: ""
 					}
 					
--- app/(frontend)/[locale]/playground/page.tsx
+++ app/(frontend)/[locale]/playground/page.tsx
@@ -1,132 +1,25 @@
-import { getTranslations } from 'next-intl/server';
-import { fetchProfile, fetchUser } from '@/app/(backend)/api/actions/user'
-import { Container, Section, Heading, DataList, Separator, Flex, Badge, Code, Card, ScrollArea, Button } from '@radix-ui/themes';
-import { ResetPasswordBtn } from '../(teacher)/(dashboard)/settings/_buttons/ResetPasswordBtn';
-import { ManageSubscriptionBtn } from '../(teacher)/(dashboard)/settings/_buttons/ManageSubscriptionBtn';
-import { SignOutBtn } from '../(teacher)/(dashboard)/settings/_buttons/SignOutBtn';
-import { SubscribeBtn } from '../(teacher)/(dashboard)/settings/_buttons/SubscribeBtn';
-import { Dot } from 'lucide-react';
-import AddOrganizationBtn from '../(teacher)/(dashboard)/settings/_buttons/AddOrganizationBtn';
+import AttendanceToPDF from "../(teacher)/(dashboard)/reports/_components/AttendanceToPDF";
 
 export default async function PlayGround () {
 	if (process.env.NODE_ENV === 'production') {
 		return (null);
 	}
-	const t = await getTranslations("settings");
-
-	const profileData = {
-		id: "blabla",
+	const date = new Date();
+	const user = {
 		first_name: "Earl",
 		last_name: "Hickey",
-		email: "ehickey@student.42.fr",
 		organization: {
-			active: false,
-			name: "The Doctor",
-			adress: "123 diitkejjh, Gallifrey"
+			name: "The Comapny",
+			address: "123 place du radeau, TREEEJHIK Cedex"
 		}
 	}
-	return (
-        <ScrollArea>
-            <Container pr='3' px={{ initial: '3', xs: '0' }}>
+	const attendance = [{
+		first_name: "Johanna",
+		last_name: "Courtois",
+		connexion: date.toDateString()
+	}];
 
-                <Section>
-
-                        <Heading as='h1' mb='2'>{t('information')}</Heading>
-
-                        <Card size='4'>
-                            <DataList.Root>
-								<Heading size='5'>Personnelles</Heading>
-                                {/*
-                                <DataList.Item>
-                                    <DataList.Label>{t("nickname")}</DataList.Label>
-                                    <DataList.Value>{nickname}</DataList.Value>
-                                </DataList.Item>*/}
-                                <DataList.Item>
-                                    <DataList.Label>{t("name")}</DataList.Label>
-                                    <DataList.Value>{profileData?.first_name || <Badge color='gray'>aucun</Badge>}</DataList.Value>
-                                </DataList.Item>
-                                <DataList.Item>
-                                    <DataList.Label>{t("surname")}</DataList.Label>
-                                    <DataList.Value>{profileData?.last_name || <Badge color='gray'>aucun</Badge>}</DataList.Value>
-                                </DataList.Item>
-                                <DataList.Item>
-                                    <DataList.Label>{t("email")}</DataList.Label>
-                                    <DataList.Value>{profileData?.email || <Badge color='gray'>aucun</Badge>}</DataList.Value>
-                                </DataList.Item>
-                                {/*<DataList.Item>
-                                    <DataList.Label>{t("id")}</DataList.Label>
-                                    <DataList.Value><Code>{user?.id}</Code></DataList.Value>
-                                </DataList.Item>*/}
-								<div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-								<Heading size='5'>Organisation</Heading>
-								{
-
-									(profileData.organization.active)
-									? <>
-										<DataList.Item>
-											<DataList.Label>{t("surname")}</DataList.Label>
-											<DataList.Value>{profileData?.organization?.name || <Badge color='gray'>aucun</Badge>}</DataList.Value>
-										</DataList.Item>
-										<DataList.Item>
-											<DataList.Label>{t("address")}</DataList.Label>
-											<DataList.Value>{profileData?.organization?.adress || <Badge color='gray'>aucun</Badge>}</DataList.Value>
-										</DataList.Item>
-									</>
-									: <AddOrganizationBtn userId={profileData.id} />
-								}
-								</div>
-                            </DataList.Root>
-
-                            <Separator size='4' my='4'/>
-
-                            <Flex gap='4' wrap='wrap'>
-                                <ResetPasswordBtn message={t("change password")}/>
-                                <SignOutBtn message={t("sign out")}/>
-                            </Flex>
-                        </Card>
-
-                </Section>
-
-                <Section>
-                    <Heading as='h1' mb='2'>{t('subscription')}</Heading>
-                    <Card size='4'>
-
-                        {/*<DataList.Root>
-                            <DataList.Item>
-                                <DataList.Label>{t("customer exists")}</DataList.Label>
-                                <DataList.Value>{customerExists ? <Badge color='green' radius='full'>yes</Badge> : <Badge color='red' radius='full'>no</Badge>}</DataList.Value>
-                            </DataList.Item>
-                            <DataList.Item>
-                                <DataList.Label>{t("stripe id")}</DataList.Label>
-                                <DataList.Value><Code>{stripe_id}</Code></DataList.Value>
-                            </DataList.Item>
-                        </DataList.Root>
-
-                        <Separator size='4' my='4'/>*/}
-
-                        <Flex gap='4'>
-                            <SubscribeBtn message={t("subscribe")} disabled={true}/>
-                            <ManageSubscriptionBtn message={t("manage subscription")} disabled={true}/>
-                        </Flex>
-                    </Card>
-                </Section>
-
-                
-                {/*<Section size='1'>
-                    <Heading as='h1' mb='2'>{'Software'}</Heading>
-                    <Card size='4'>
-
-                        <DataList.Root>
-                            <DataList.Item>
-                                <DataList.Label>{"Supabase URL"}</DataList.Label>
-                                <DataList.Value>{process.env.NEXT_PUBLIC_SUPABASE_URL}</DataList.Value>
-                            </DataList.Item>
-                        </DataList.Root>
-
-                    </Card>
-                </Section>*/}
-
-            </Container>
-        </ScrollArea>
-    )
-}
+	return (<>
+		<AttendanceToPDF attendances={attendance} sessionDate={date.toISOString()} capsuleTitle="Hola que tal ?" user={{userInfo: user, roomId: "1234"}}/>
+	</>)
+};
