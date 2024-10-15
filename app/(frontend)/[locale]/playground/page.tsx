a5deb5f0d8d4579111b98ac95ec3a6f4d47a5bde
Johanna <jcourtoi@student.42.fr>
2024-10-15 11:07:47 +0200

    ADD end_of_session on rooms table and pdf

--- app/(frontend)/[locale]/playground/page.tsx
+++ app/(frontend)/[locale]/playground/page.tsx
@@ -1,25 +1,9 @@
-import AttendanceToPDF from "../(teacher)/(dashboard)/reports/_components/AttendanceToPDF";
 
 export default async function PlayGround () {
 	if (process.env.NODE_ENV === 'production') {
 		return (null);
 	}
-	const date = new Date();
-	const user = {
-		first_name: "Earl",
-		last_name: "Hickey",
-		organization: {
-			name: "The Comapny",
-			address: "123 place du radeau, TREEEJHIK Cedex"
-		}
-	}
-	const attendance = [{
-		first_name: "Johanna",
-		last_name: "Courtois",
-		connexion: date.toDateString()
-	}];
 
 	return (<>
-		<AttendanceToPDF attendances={attendance} sessionDate={date.toISOString()} capsuleTitle="Hola que tal ?" user={{userInfo: user, roomId: "1234"}}/>
 	</>)
 };
