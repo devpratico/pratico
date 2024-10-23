f64ad2356a6d76f718090f66b6ffa1329cec6ed4
Johanna <jcourtoi@student.42.fr>
2024-10-23 16:45:26 +0200

    FIX server error

--- app/(frontend)/[locale]/(teacher)/(desk)/room/[room_code]/page.tsx
+++ app/(frontend)/[locale]/(teacher)/(desk)/room/[room_code]/page.tsx
@@ -18,14 +18,15 @@ export default async function Page({ params: { room_code } }: { params: { room_c
 	const { data: { user }, error: userError } = await supabase.auth.getUser();
     if (!user || userError) {
         logger.log('next:page', 'User info missing or error fetchingUser', userError ? userError : null);
+		redirect(`/classroom/${room_code}`);
         return (null);
     }
-    const { data: roomData, error: roomError } = await supabase.from('rooms').select('created_by').single();
+    const { data: roomData, error: roomError } = await supabase.from('rooms').select('created_by').limit(1).single();
     if (roomError) {
-        logger.log("next:page", "StudentViewPage", "room error", roomError);
+        logger.log("next:page", "TeacherViewPage", "room error", roomError);
         throw (roomError);
     }
-    if (user.id !== roomData?.created_by)
+    if (user?.id !== roomData?.created_by)
         redirect(`/classroom/${room_code}`);
     return (
         <>
