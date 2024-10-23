05cee388dc5c090c1de1615faf7f53c84aa4c815
Thomas DOUCHE <Thomas.douche@aol.com>
2024-10-23 17:05:33 +0200

    make logger pick correct sendDiscordMessage depending on env

--- app/(frontend)/[locale]/playground/page.tsx
+++ app/(frontend)/[locale]/playground/page.tsx
@@ -1,9 +1,17 @@
+'use server'
+import sendDiscordMessage from "@/app/(backend)/api/discord/send-message/server";
+
 
 export default async function PlayGround () {
 	if (process.env.NODE_ENV === 'production') {
 		return (null);
 	}
 
+    const { success, error } = await sendDiscordMessage('Hello from server');
+
 	return (<>
+
+    <p>Hello</p>
+
 	</>)
 };
