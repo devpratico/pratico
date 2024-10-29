0fe61725f760463ad6967b763c4c35ebff07a037
Johanna <jcourtoi@student.42.fr>
2024-10-29 13:01:59 +0100

    UPDATE dnd kit

--- app/(frontend)/[locale]/(teacher)/(desk)/_components/Draggable.tsx
+++ app/(frontend)/[locale]/(teacher)/(desk)/_components/Draggable.tsx
@@ -1,28 +1,19 @@
 import { useDraggable } from "@dnd-kit/core";
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 
 export function Draggable({ children, id }: { children?: React.ReactNode; id: string }) {
 	const { attributes, listeners, setNodeRef, transform } = useDraggable({ id });
 	const style = transform ? { transform: `translate3d(${transform.x}px, ${transform.y}px, 0)` } : undefined;
-
 	const [isDragging, setIsDragging] = useState(false);
-	const isDraggingRef = useRef(false);
 
 	const handleMouseDown = () => {
-		isDraggingRef.current = false;
-
-		const handleMouseMove = () => {
-			isDraggingRef.current = true;
-			setIsDragging(true);
-			document.removeEventListener('mousemove', handleMouseMove);
-		};
-
-		document.addEventListener('mousemove', handleMouseMove);
-		document.addEventListener('mouseup', () => {
+		setIsDragging(true);
+		const handleMouseUp = () => {
 			setIsDragging(false);
-			document.removeEventListener('mousemove', handleMouseMove);
-		});
-	};
+			document.removeEventListener('mouseup', handleMouseUp);
+		};
+		document.addEventListener('mouseup', handleMouseUp);
+	}
 
 	return (
 		<div
--- app/(frontend)/[locale]/(teacher)/(desk)/_components/Droppable.tsx
+++ app/(frontend)/[locale]/(teacher)/(desk)/_components/Droppable.tsx
@@ -2,7 +2,7 @@ import { useDroppable } from "@dnd-kit/core";
 
 export function Droppable ({children, id}: {children?: React.ReactNode, id: string}) {
 	const { isOver, setNodeRef } = useDroppable({id: id});
-	const style = { backgroundColor: isOver ? 'var(--gray-5)' : undefined};
+	const style = { backgroundColor: isOver ? 'var(--violet-5)' : undefined};
 
 	return (<div ref={setNodeRef} style={style}>
 		{children}
