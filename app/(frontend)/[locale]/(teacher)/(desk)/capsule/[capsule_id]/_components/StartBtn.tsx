00109750ffbc8996596dbd2acf9bcb976013a2cb
Johanna <jcourtoi@student.42.fr>
2024-11-04 13:52:25 +0100

    UPDATE the last active session can be retrieve

--- app/(frontend)/[locale]/(teacher)/(desk)/capsule/[capsule_id]/_components/StartBtn.tsx
+++ app/(frontend)/[locale]/(teacher)/(desk)/capsule/[capsule_id]/_components/StartBtn.tsx
@@ -3,9 +3,10 @@ import logger from "@/app/_utils/logger";
 import { createRoom } from "@/app/(backend)/api/room/room.client";
 import { useParams } from "next/navigation";
 import { useRouter } from "@/app/(frontend)/_intl/intlNavigation";
-import { Button, Box } from "@radix-ui/themes";
+import { Button } from "@radix-ui/themes";
 import { useState } from "react";
 import { Play } from "lucide-react";
+import createClient from "@/supabase/clients/client";
 
 
 interface StartBtnProps {
@@ -17,6 +18,7 @@ export default function StartBtn({ message, variant='surface' }: StartBtnProps) 
     const { capsule_id: capsuleId } = useParams<{ capsule_id: string }>()
     const router = useRouter()
     const [loading, setLoading] = useState(false)
+	const supabase = createClient();
 
 
     const handleClick = async () => {
@@ -25,11 +27,29 @@ export default function StartBtn({ message, variant='surface' }: StartBtnProps) 
         if (!capsuleId) return logger.error('supabase:database', 'No capsule id provided for start button')
     
         setLoading(true)
-        // Start the session and get the room that is created
-        const { room: createdRoom, error} = await createRoom(capsuleId)
+		const { data: existedRoomData, error: existedRoomError } = await supabase.from("rooms").select("*").eq("status", "open");
+		if (existedRoomError)
+			logger.error("react:component", "StartBtn for session", "error while getting data", existedRoomError);
+		logger.log("react:component",  "StartBtn for session data", existedRoomData);
 
-        // Redirect to the room page
-        if (createdRoom) router.push(`/room/${createdRoom.code}`)
+		if (!existedRoomData?.length)
+		{
+			// Start the session and get the room that is created
+			const { room: createdRoom, error} = await createRoom(capsuleId)
+
+			// Redirect to the room page
+			if (createdRoom) router.push(`/room/${createdRoom.code}`)
+		}
+		else
+		{
+			const mostRecentItem = existedRoomData.sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime())[0];
+
+			if (mostRecentItem)
+				router.push(`/room/${mostRecentItem.code}`);
+			else
+				logger.error("react:component", "StartBnt session", "Error while getting the most recent active session", "discord");
+		}
+  
     }
 
     return (
