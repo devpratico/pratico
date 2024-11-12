565bf0f0f280bbff8762eddce827799b5f9ea6ef
Johanna <jcourtoi@student.42.fr>
2024-11-12 16:02:26 +0100

    UPDATE I have a rect in all the new pages

--- app/(frontend)/[locale]/playground/page.tsx
+++ app/(frontend)/[locale]/playground/page.tsx
@@ -1,9 +1,10 @@
 'use client';
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { createShapeId, Editor, exportToBlob, IndexKey, Tldraw, TLFrameShape, TLPageId, TLParentId, TLShapeId, TLUiComponents, uniqueId, useEditor } from "tldraw";
 import 'tldraw/tldraw.css'
 import { useTLEditor } from "../../_hooks/useTLEditor";
 import { CardShapeUtil } from "./_components/ShapeUtilClass";
+import { useNav } from "../../_hooks/useNav";
 
 const MyCustomShapes = [CardShapeUtil]
 
@@ -11,17 +12,23 @@ export default function PlayGround () {
 	// if (process.env.NODE_ENV === 'production') {
 	// 	return (null);
 	// }
-	const { editor, setEditor } = useTLEditor();
+	const [ editor, setEditor ] = useState<Editor>();
+	const [ currentPageId, setCurrentPageId ] = useState<TLPageId>();
+	const [ pageIds, setPageIds ] = useState<TLPageId[] | undefined>([]); 
 
-    const handleOnMount = useCallback((editor: Editor) => {
-		setEditor(editor);
-		
-        editor?.createPage({
-			id: `page:${uniqueId()}` as TLPageId
-        })
-	
-		// editor.createShapes([{type: 'card'}]);
-		editor.sideEffects.registerAfterCreateHandler('page', (newpage) => {
+	useEffect(() => {
+		console.log("useeffect", editor);
+		if (!editor)
+			return ;
+		console.log("Editor exists", editor);
+
+		const searchId = pageIds?.find((item) => {
+			return (item === currentPageId);
+		});
+		console.log("searchId", searchId, currentPageId);
+
+		if ((!searchId || !searchId.length) && currentPageId)
+		{
 			const { x,y,w,h } = editor.getViewportPageBounds();
 			editor.createShape({type: 'card',
 				x: x,
@@ -33,7 +40,47 @@ export default function PlayGround () {
 					color: 'var(--violet-8)'
 				}
 			});
+			setPageIds((prev) => [...(prev || []), currentPageId]);
+			console.log(pageIds);
+		}
+		
+		// });
+	}, [editor, currentPageId, pageIds]);
+	
+    const handleOnMount = useCallback((editor: Editor) => {
+		if (editor)
+			setEditor(editor);
+		console.log("Editor handleOnMount", editor);
+      
+		const { x,y,w,h } = editor.getViewportPageBounds();
+		editor.createShape({type: 'card',
+			x: x,
+			y: y,
+			isLocked: true,
+			props: {
+				w: w,
+				h: h,
+				color: 'var(--violet-8)'
+			}
+		});
+		editor?.sideEffects.registerAfterCreateHandler('page', (newpage) => {
 
+			console.log("NEW PAGE", newpage);
+			setCurrentPageId(newpage.id);
+			console.log("CURRENT", currentPageId);
+
+			editor.createShape({type: 'card',
+				x: x,
+				y: y,
+				isLocked: true,
+				props: {
+					w: w,
+					h: h,
+					color: 'var(--violet-8)'
+				}
+			});
+		});
+		
 	
 			// editor?.createShape<TLFrameShape>({
 			// 		"parentId": "page:somePage" as TLParentId,
@@ -52,7 +99,7 @@ export default function PlayGround () {
 			// 		},
 			// 		"meta": {},
 			//  })
-		})
+		// })
 		// editor.createShape({
 		// 	type: 'text',
 		// 	x: 200,
@@ -67,7 +114,9 @@ export default function PlayGround () {
 		// editor.zoomToSelection({
 		// 	animation: { duration: 5000 },
 		// })
-    }, [setEditor]);
+    }, [setEditor, currentPageId]);
+
+
 
 	const components: TLUiComponents = {
 		SharePanel: ExportCanvasButton,
@@ -99,7 +148,7 @@ function ExportCanvasButton() {
 				if (shapeIds.size === 0) return alert('No shapes on the canvas')
 				const blob = await exportToBlob({
 					editor,
-					ids: [...shapeIds],
+					ids: [...shapeIds], // downlevelIteration: true dans tsconfig permet des constructions ES6 même lors de la transpilation vers des versions plus anciennes (comme es5).
 					format: 'png',
 					opts: { background: false },
 				})
