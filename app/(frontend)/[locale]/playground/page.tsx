4c1d91ee59d1557ec41200f23f0586113f0e3fc2
Johanna <jcourtoi@student.42.fr>
2024-11-13 17:24:22 +0100

    SAVE jspdf svg to pdf, doesnt work yet

--- app/(frontend)/[locale]/playground/page.tsx
+++ app/(frontend)/[locale]/playground/page.tsx
@@ -227,16 +227,36 @@ function ExportCanvasButton() {
 	)
 }
 
-import jspdf from "jspdf";
+import {jsPDF} from "jspdf";
 
 const ExportAllPagesButton = () => {
-  const editor = useEditor();
-  const [svgPages, setSvgPages] = useState<any[]>([]);
+	const editor = useEditor();
+	const [svgPages, setSvgPages] = useState<any[]>([]);
+	const pdf = new jsPDF('p', 'mm', 'a4');
+
+ 	const addSvgToPdf = async (svgString: string, blob: Blob) => {
+	// const svgBlob = new Blob([svgString], { type: 'image/svg+xml;charset=utf-8' });
+	const url = URL.createObjectURL(blob);
+  
+	const img = new Image();
+	img.src = url;
+  
+	img.onload = () => {
+  
+	  pdf.addSvgAsImage(url, 0, 0, 210, 297);
+  
+  
+	  URL.revokeObjectURL(url);
+	};
+  
+	img.onerror = (error) => {
+	  console.error('Failed to load the SVG image:', error);
+	};
+  };
 
   const handleExportAllPages = async () => {
     const allSVGs: any[] = [];
     const allPages = editor.getPages();
-    const pdf = new jspdf();
 
     if (allPages.length === 0) return;
 
@@ -248,27 +268,28 @@ const ExportAllPagesButton = () => {
       try {
         const svg = await editor.getSvgElement(Array.from(shapeIds));
         const svgString = await editor.getSvgString(Array.from(shapeIds));
+		const blob = await exportToBlob({
+			editor,
+			ids: Array.from(shapeIds),
+			format: 'svg',
+			opts: { background: false },
+		})
 
         allSVGs.push(svg);
 
-        if (pdf.internal.pages.length > 1) {
-          pdf.addPage();
-        }
-
         if (svgString) {
 			console.log(svgString);
-          const imgData = `data:image/svg+xml;base64,${btoa(svgString.svg)}`;
-          pdf.addImage(imgData, "SVG", 0, 0, 210, 297);
+          addSvgToPdf(svgString.svg, blob);
         }
       } catch (error) {
         console.error(`Failed to get svgElement in page ${page.id}`, error);
       }
     }
+	pdf.save('output.pdf');
 
     setSvgPages(allSVGs);
     console.log(allSVGs);
 
-    pdf.save("exported_pages.pdf");
   };
 
   return (
