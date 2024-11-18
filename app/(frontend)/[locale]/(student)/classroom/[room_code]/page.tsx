ce0de5111cd5fe56be71772beeda6707f4b7dd3e
Johanna <jcourtoi@student.42.fr>
2024-11-18 15:47:29 +0100

    UPDATE verif limit participants

--- app/(frontend)/[locale]/(student)/classroom/[room_code]/page.tsx
+++ app/(frontend)/[locale]/(student)/classroom/[room_code]/page.tsx
@@ -12,16 +12,6 @@ import { sendDiscordMessage } from '@/app/(backend)/api/discord/discord.server'
 
 
 export default async function StudentViewPage({ params }: { params: { room_code: string } }) {
-
-    // Check user is logged in (can be anonymous)
-    const { user, error: userError } = await fetchUser();
-    if (!user || userError) {
-        logger.log('next:page', 'User info missing or error fetchingUser, redirecting to form', userError ? userError : null);
-        const nextUrl = `/classroom/${params.room_code}`;
-        redirect('/form?' + new URLSearchParams({ nextUrl }).toString());
-        return (null);
-    }
-    
     // Check room exists
 	const { data: roomData, error: roomError } = await fetchOpenRoomByCode(params.room_code);
     if (roomError || !roomData) {
@@ -29,37 +19,70 @@ export default async function StudentViewPage({ params }: { params: { room_code:
 		throw (roomError);
 	}
 
+	// Check teacher is a paid customer
+	const isPaidCustomer = await roomCreatorIsPaidCustomer(roomData.id);
+	let limitReached = false;
+	const maxParticipants = 10;
+	const attendanceCount = await countAttendances(roomData.id);
+	if (!isPaidCustomer) {
+	// Only 10 participants are allowed for free customers.
+
+		logger.log('next:page', 'StudentViewPage', 'attendance count', attendanceCount);
+		if (attendanceCount >= maxParticipants) {
+			limitReached = true;
+		}
+	} else {
+		limitReached = false;
+	}
+
+	// Check user is logged in (can be anonymous)
+	const { user, error: userError } = await fetchUser();
+	if (!user || userError) {
+		logger.log('next:page', 'User info missing or error fetchingUser, redirecting to form', userError ? userError : null);
+		if (limitReached) {
+			logger.log('next:page', 'StudentViewPage', 'attendance count is greater than 10. Blocking user.');
+			const { data: creatorData, error: creatorError } = await fetchRoomCreator(roomData!.code!)//roomData!.id);
+			const creatorId = creatorData?.created_by;
+			if (creatorId) {
+				//const creatorName = (await getProfile(creatorId)
+				const { data } = await fetchProfile(creatorId);
+				let creatorName = (data?.first_name || '')  + ' ' + (data?.last_name || '');
+				if (creatorName === ' ') creatorName = 'un utilisateur anonyme';
+				await sendDiscordMessage(`🚪 **Limite de Pratico Free** atteinte (${maxParticipants} participants) pour ${creatorName} dans la salle ${params.room_code} !`);
+			}
+			throw new Error('Le nombre maximum de participants est atteint (10). Veuillez contacter l\'organisateur pour obtenir un accès.');
+		}
+		else {
+			const nextUrl = `/classroom/${params.room_code}`;
+			redirect('/form?' + new URLSearchParams({ nextUrl }).toString());
+			return (null);
+		}
+	}
+
 	// Check user has signed attendance
 	logger.log('supabase:database', 'page classroom fetchOpenRoom id:', roomData.id);
 	const { data , error  } = await fetchUserHasSignedAttendance(roomData.id, user.id);
 	logger.log('supabase:database', 'fetUserHasSignedAttendance', data);
+
 	if (!data || !data?.first_name || !data?.last_name || error) {
 		logger.log('next:page', 'Attendance one of the names or both not found, redirecting to form', error ? error : null);
-		// Check teacher is a paid customer
-		const isPaidCustomer = await roomCreatorIsPaidCustomer(roomData.id);
-		if (!isPaidCustomer) {
-		// Only 10 participants are allowed for free customers.
-			const maxParticipants = 10;
-			const attendanceCount = await countAttendances(roomData.id);
-			logger.log('next:page', 'StudentViewPage', 'attendance count', attendanceCount);
-			if (attendanceCount >= maxParticipants) {
-				logger.log('next:page', 'StudentViewPage', 'attendance count is greater than 10. Blocking user.');
-
-				const { data: creatorData, error: creatorError } = await fetchRoomCreator(roomData!.code!)//roomData!.id);
-				const creatorId = creatorData?.created_by;
-				if (creatorId) {
-					//const creatorName = (await getProfile(creatorId)
-					const { data } = await fetchProfile(creatorId);
-					let creatorName = (data?.first_name || '')  + ' ' + (data?.last_name || '');
-					if (creatorName === ' ') creatorName = 'un utilisateur anonyme';
-					await sendDiscordMessage(`🚪 **Limite de Pratico Free** atteinte (${maxParticipants} participants) pour ${creatorName} dans la salle ${params.room_code} !`);
-				}
-				throw new Error('Le nombre maximum de participants est atteint (10). Veuillez contacter l\'organisateur pour obtenir un accès.');
-			} else {
-				const nextUrl = `/classroom/${params.room_code}`;
-				redirect('/form?' + new URLSearchParams({ nextUrl }).toString());
-				return (null);
+		if (limitReached) {
+			logger.log('next:page', 'StudentViewPage', 'attendance count is greater than 10. Blocking user.');
+			const { data: creatorData, error: creatorError } = await fetchRoomCreator(roomData!.code!)//roomData!.id);
+			const creatorId = creatorData?.created_by;
+			if (creatorId) {
+				//const creatorName = (await getProfile(creatorId)
+				const { data } = await fetchProfile(creatorId);
+				let creatorName = (data?.first_name || '')  + ' ' + (data?.last_name || '');
+				if (creatorName === ' ') creatorName = 'un utilisateur anonyme';
+				await sendDiscordMessage(`🚪 **Limite de Pratico Free** atteinte (${maxParticipants} participants) pour ${creatorName} dans la salle ${params.room_code} !`);
 			}
+			throw new Error('Le nombre maximum de participants est atteint (10). Veuillez contacter l\'organisateur pour obtenir un accès.');
+		}
+		else {
+			const nextUrl = `/classroom/${params.room_code}`;
+			redirect('/form?' + new URLSearchParams({ nextUrl }).toString());
+			return (null);
 		}
 	}
 
