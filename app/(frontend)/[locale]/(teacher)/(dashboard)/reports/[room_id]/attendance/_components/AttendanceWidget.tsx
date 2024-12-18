c00031ab0b501ddebf8e02a2a52692f099549dc6
Johanna <jcourtoi@student.42.fr>
2024-12-18 12:34:16 +0100

    TEST format date with explicit timeszone

--- app/(frontend)/[locale]/(teacher)/(dashboard)/reports/[room_id]/attendance/_components/AttendanceWidget.tsx
+++ app/(frontend)/[locale]/(teacher)/(dashboard)/reports/[room_id]/attendance/_components/AttendanceWidget.tsx
@@ -4,7 +4,7 @@ import { countAttendances } from "@/app/(backend)/api/attendance/attendance.serv
 import logger from "@/app/_utils/logger";
 import { Json } from "@/supabase/types/database.types";
 import { AttendanceInfoType } from "../../page";
-import { getFormatter, getLocale } from "next-intl/server";
+import { getFormatter } from "next-intl/server";
 
 type AttendanceWidgetProps = {
 	roomId: number,
@@ -15,7 +15,6 @@ type AttendanceWidgetProps = {
 export async function AttendanceWidget({ roomId, userId, capsuleTitle }: AttendanceWidgetProps) {
 	const supabase = createClient();
 	const formatter = await getFormatter();
-	const local = await getLocale();
 	const attendanceCount = await countAttendances(roomId);
 	let sessionDate: { date: Date, end: Date } | null = null;
 	let data: AttendanceWidgetViewProps["data"];
@@ -63,8 +62,7 @@ export async function AttendanceWidget({ roomId, userId, capsuleTitle }: Attenda
 					const infos: AttendanceInfoType = {
 						first_name: attendance.first_name,
 						last_name: attendance.last_name,
-						connexion: new Intl.DateTimeFormat(local, {timeStyle: 'short', timeZone: 'local'}).format(new Date(attendance.created_at))
-						//formatter.dateTime(new Date(attendance.created_at), {timeStyle:'short', timeZone: 'local'})
+						connexion: formatter.dateTime(new Date(attendance.created_at), {timeStyle:'short', timeZone: 'Europe/Paris'})
 					};
 					attendances.push(infos);
 				})
