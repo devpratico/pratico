d55af577e7bece6e950ffa1c27c5b49274aab104
Johanna <jcourtoi@student.42.fr>
2024-12-30 13:43:48 +0100

    TEST with supabase direclty

--- app/(frontend)/[locale]/(teacher)/(dashboard)/reports/[room_id]/_components/CapsuleWidget.tsx
+++ app/(frontend)/[locale]/(teacher)/(dashboard)/reports/[room_id]/_components/CapsuleWidget.tsx
@@ -2,7 +2,6 @@ import createClient from "@/supabase/clients/server";
 import { CapsuleWidgetView } from "./CapsuleWidgetView";
 import { getFormatter } from "next-intl/server";
 import { TLEditorSnapshot } from "tldraw";
-import { fetchRoomsByCapsuleId } from "@/app/(backend)/api/room/room.server";
 import { Json } from "@/supabase/types/database.types";
 import logger from "@/app/_utils/logger";
 
@@ -12,6 +11,7 @@ export async function CapsuleWidget ({ userId, capsuleTitle, capsuleId }: { user
 	let capsuleDate = "";
 	let capsuleSnapshot: Json | TLEditorSnapshot | null = null;
 	let isRoom = false;
+	logger.log('supabase:database', 'CapsuleWidget', 'userId', userId, 'capsuleId', capsuleId);
 	if (!capsuleId)
 		throw new Error("capsuleId is missing");
 	const { data: capsuleData } = await supabase.from('capsules').select('tld_snapshot, created_at').eq("created_by", userId).eq('id', capsuleId).single();
@@ -21,11 +21,10 @@ export async function CapsuleWidget ({ userId, capsuleTitle, capsuleId }: { user
 		capsuleDate = formatter.dateTime(date, { dateStyle: 'short' });
 		if (capsuleData.tld_snapshot && capsuleData.tld_snapshot?.length > 0)
 			capsuleSnapshot = capsuleData.tld_snapshot[0];
-		const { data: roomData } = await fetchRoomsByCapsuleId(capsuleId);
+		const { data: roomData } = await supabase.from('rooms').select('status').eq("id", userId).eq('capsule_id', capsuleId).single();
 		if (roomData)
 		{
-			if (roomData.length > 0)
-				isRoom = roomData[0].status === "open";
+			isRoom = roomData.status === "open";
 		}
 		else
 			logger.error('supabase:database', 'CapsuleWidget', 'Error getting room data');
