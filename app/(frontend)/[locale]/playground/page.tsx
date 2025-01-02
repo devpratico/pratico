afb1376cf84623534af4dd13315d05306414ebb1
Johanna <jcourtoi@student.42.fr>
2025-01-02 08:46:11 +0100

    RM playground

--- app/(frontend)/[locale]/playground/page.tsx
+++ app/(frontend)/[locale]/playground/page.tsx
@@ -1,49 +1,15 @@
 "use client";
 
-import createClient from "@/supabase/clients/client";
-import { Json } from "@/supabase/types/database.types";
 import { Box } from "@radix-ui/themes";
-import { useEffect, useState } from "react";
-import { TldrawImage, TLEditorSnapshot } from "tldraw";
+
 
 export default function PlayGround () {
-	const [ snapshot, setSnapshot ] = useState<Json | TLEditorSnapshot>();
 	if (process.env.NODE_ENV === 'production') {
 		return (null);
 	}
-	const supabase = createClient();
-
-	useEffect(() => {
-		const getData = async () => {
-			console.log("Playground getData");
-			const { data: { user } } = await supabase.auth.getUser();
-		
-			if (!user)
-				return (console.log("USER NOT FOUND IN THE PLAYGROUND"));
-			const { data, error } = await supabase.from("rooms").select("*").eq("created_by", user.id);
-			if (data)
-			{
-				console.log("HERE THE DATA:", data);
-				if (data.length === 2)
-					setSnapshot(data[1].capsule_snapshot);
-				else
-					setSnapshot(data[0].capsule_snapshot);
-			}
-			else
-			{
-				if (error)
-					console.error("HERE THE ERROR:", error);
-				else
-					console.log("NOTHING, NO DATA NOR ERROR");
-			}
-		};
-		getData();
-	}, []);
-
 
 	return (
 		<Box width="auto">
-			<TldrawImage snapshot={snapshot as TLEditorSnapshot} />
 		</Box>
 	)
 };
