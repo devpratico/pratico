6de473b3cd1b1905fc8ee6aff98fe9cdb5a93c52
Thomas DOUCHE <Thomas.douche@aol.com>
2025-01-02 18:04:15 +0100

    fix layout and simplify logic

--- app/(frontend)/[locale]/(teacher)/(dashboard)/reports/[room_id]/page.tsx
+++ app/(frontend)/[locale]/(teacher)/(dashboard)/reports/[room_id]/page.tsx
@@ -1,5 +1,5 @@
 import logger from "@/app/_utils/logger";
-import { Container, Flex, Grid, ScrollArea, Section, Text } from "@radix-ui/themes";
+import { Container, Flex, Grid, ScrollArea, Section, Text, Heading } from "@radix-ui/themes";
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 import createClient from "@/supabase/clients/server";
 import { AttendanceWidget } from "./attendance/_components/AttendanceWidget";
@@ -25,67 +25,81 @@ export type SessionInfoType = {
 };
 
 export default async function SessionDetailsPage ({ params }: { params: Params }) {
-	const supabase = createClient();
 	const roomId: string = params.room_id;
-	const formatter = await getFormatter();
-	let userId: string | null = null;
-	let capsuleTitle = "Sans titre";
-	let capsuleId: string | null = null;
-	let sessionDate: { date: Date, end: Date } | null = null;
-	if (!(roomId))
-	{
-		logger.error("next:page", "SessionDetailsPage", "roomId or capsuleId missing");
-		return ;				
-	}
-	try {
-		const {data: { user }} = await supabase.auth.getUser();
-		if (user)
-		{
-			userId = user.id;
-			const {data: roomData, error: roomError} = await supabase.from('rooms').select('created_at, capsule_id, end_of_session').eq('id', roomId).single();
-			if (roomData && roomData.end_of_session)
-				sessionDate = { date: new Date(roomData.created_at), end: new Date(roomData.end_of_session) };
-			if (roomData?.capsule_id)
-			{
-				capsuleId = roomData?.capsule_id;
-				const { data: capsuleData } = await supabase.from('capsules').select('title').eq('id', capsuleId).single();
-				if (capsuleData)
-					capsuleTitle = capsuleData.title ? capsuleData.title : "Sans titre";
-			}
-		}
-	} catch (err) {
-        logger.error('supabase:database', 'CapsuleSessionsReportServer', 'Error getting attendances', err);
-	}
-	if (!userId)
-	{	
-		logger.error('supabase:database', 'CapsuleSessionsReportServer', 'User not found');
-		throw new Error("L'utilisateur n'a pas été trouvé");
-	}
-	return (<>
-		<ScrollArea type="scroll"> 
-			<Section px={{ initial: '3', xs: '0' }}>
-				<Container>
-					<Flex direction="column" gap="3" align="start">
-						<BackButton backTo="/reports"/>
-						<Text size="4" weight="bold">
-						{		
-							capsuleTitle && capsuleTitle !== "Sans titre"
-							? capsuleTitle
-							: "Capsule sans titre"
-						}
-						</Text>
-						{
-							sessionDate?.date
-							? <Text size="1">{`Session du ${formatter.dateTime(sessionDate.date, {dateStyle: "short"})}`}</Text> 
-							: <></>
-						}
-					</Flex>
-					<Grid columns='repeat(auto-fill, minmax(400px, 1fr))' gap='3' p='5'>
-						<AttendanceWidget roomId={roomId} userId={userId!} capsuleTitle={capsuleTitle}/>
-						<CapsuleWidget userId={userId} capsuleTitle={capsuleTitle} capsuleId={capsuleId} roomId={roomId} />
-					</Grid>
-				</Container>
-			</Section>	
+
+    let capsuleTitle = "Sans titre";
+    let sessionDateSubtitle = "Date inconnue";
+
+    const capsuleId = await getCapsuleId(roomId);
+
+    if (capsuleId) {
+        capsuleTitle = await getCapsuleTitle(capsuleId) ?? capsuleTitle;
+    }
+
+    const sessionDate = await getSessionStartDate(roomId);
+
+    if (sessionDate) {
+        const formatter = await getFormatter();
+        sessionDateSubtitle = `Session du ${formatter.dateTime(sessionDate, {dateStyle: "short"})}`;
+    }
+
+	return (
+		<ScrollArea>
+			<Container>
+
+                <Section size='1'>
+                    <BackButton backTo="/reports" />
+                </Section>
+
+                <Section size='2'>
+
+                    <Heading as='h1' size="7" color='violet'>{capsuleTitle}</Heading>
+                    <Text size="2" color='gray'>{sessionDateSubtitle}</Text> 
+
+                    <Grid columns='repeat(auto-fill, minmax(400px, 1fr))' gap='3' mt='8'>
+                        <AttendanceWidget roomId={roomId} capsuleTitle={capsuleTitle}/>
+                        <CapsuleWidget capsuleTitle={capsuleTitle} capsuleId={capsuleId} roomId={roomId} />
+                    </Grid>
+
+                </Section>
+                
+			</Container>
 		</ScrollArea>
-	</>);
+	);
 };
+
+
+// TODO: put the following functions inside the relevant _.server.ts file,
+// with proper error handling (returning a { data, error } object)
+
+async function getCapsuleId(roomId: string) {
+    const supabase = createClient();
+    const { data, error } = await supabase.from('rooms').select('capsule_id').eq('id', roomId).single();
+    if (error || !data.capsule_id) {
+        logger.error('next:page', 'SessionDetailsPage', 'Error fetching capsuleId', error?.message ?? 'No data');
+        return null
+    }
+    return data.capsule_id
+}
+
+
+async function getCapsuleTitle(capsuleId: string) {
+    const supabase = createClient();
+    const { data, error } = await supabase.from('capsules').select('title').eq('id', capsuleId).single();
+    if (error || !data.title) {
+        logger.error('next:page', 'SessionDetailsPage', 'Error fetching capsuleTitle', error?.message ?? 'No data');
+        return null
+    }
+    return data.title
+}
+
+
+async function getSessionStartDate(roomId: string) {
+    const supabase = createClient();
+    const { data, error } = await supabase.from('rooms').select('created_at').eq('id', roomId).single();
+    if (error || !data.created_at) {
+        logger.error('next:page', 'SessionDetailsPage', 'Error fetching session date', error?.message ?? 'No data');
+        return null
+    }
+    return new Date(data.created_at)
+}
