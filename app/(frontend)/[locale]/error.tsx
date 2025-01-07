0a5067843fee84f3c1e74069e693c3bde095ed71
Johanna <jcourtoi@student.42.fr>
2025-01-07 09:45:17 +0100

    UPDATE error.tsx back to digest

--- app/(frontend)/[locale]/error.tsx
+++ app/(frontend)/[locale]/error.tsx
@@ -27,8 +27,7 @@ export default function Error({ error, reset}: { error: Error & { digest?: strin
 								</Callout.Icon>
 								<Callout.Text>
 									{
-                                        // process.env.NODE_ENV === 'development' ? error.message : error.digest
-										error.message
+                                        process.env.NODE_ENV === 'development' ? error.message : error.digest
 									}
 								</Callout.Text>
 							</Callout.Root>
