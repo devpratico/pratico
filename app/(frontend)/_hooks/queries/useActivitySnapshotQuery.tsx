f19edcdbd0f94224863e18d2caf6d80ae5f11c0f
Thomas DOUCHE <Thomas.douche@aol.com>
2025-01-08 14:57:17 +0100

    new sapshot query hook

---
+++ app/(frontend)/_hooks/queries/useActivitySnapshotQuery.tsx
@@ -0,0 +1,48 @@
+'use client'
+import { PollSnapshot } from "@/app/_types/poll"
+import { QuizSnapshot } from "@/app/_types/quiz"
+import { PostgrestError } from "@supabase/supabase-js"
+import useFetch from "./useFetch"
+import createClient from "@/supabase/clients/client"
+
+
+type ReturnedType = {
+    data: QuizSnapshot | PollSnapshot
+    error: null
+} | {
+    data: null
+    error: PostgrestError | Error
+}
+
+export default function useActivitySnapshotQuery(): {
+    fetchSnapshot: (roomId: string) =>Promise<ReturnedType>
+    isPending: boolean
+} {
+    const { fetch, isPending } = useFetch(async (roomId: string) => {
+        const supabase = createClient()
+        const { data, error } = await supabase
+            .from('rooms')
+            .select('activity_snapshot')
+            .eq('id', roomId)
+            .single()
+
+        if (error) return { data: null, error: error }
+
+        //return { data: data?.activity_snapshot as QuizSnapshot | PollSnapshot, error: null }
+        const snapshot = data.activity_snapshot
+
+        if (!snapshot || typeof snapshot !== 'object' || !('type' in snapshot)) {
+            return { data: null, error: new Error('Invalid snapshot data') };
+        }
+
+        if (snapshot.type === 'quiz') {
+            return { data: snapshot as unknown as QuizSnapshot, error: null };
+        } else if (snapshot.type === 'poll') {
+            return { data: snapshot as unknown as  PollSnapshot, error: null };
+        } else {
+            return { data: null, error: new Error('Unknown snapshot type') };
+        }
+    })
+
+    return { fetchSnapshot: fetch, isPending }
+}
