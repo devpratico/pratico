b754a63bc7d3e4007bd596e0104527f9399b5dfd
Johanna <jcourtoi@student.42.fr>
2025-01-15 14:41:45 +0100

    UPDATE fetch rooms only open ones

--- app/(backend)/api/attendance/attendance.server.ts
+++ app/(backend)/api/attendance/attendance.server.ts
@@ -9,59 +9,58 @@ export type AttendanceInsert = TablesInsert<'attendance'>
 export const fetchAllAttendances = async () => {
     const supabase = createClient();
     const { data, error } = await supabase.from('attendance').select();
-    if (error) logger.error('supabase:database', `error fetching all attendances.`, error.message)
+    if (error) logger.error('supabase:database', 'attendance.server', `error fetching all attendances.`, error.message)
     return ({ data, error: error?.message });
 };
 
 export const fetchAttendance = async (id: number | undefined) => {
     if (!id) {
-        logger.error('next:api', 'fetchAttendance id missing');
+        logger.error('next:api', 'attendance.server', 'fetchAttendance id missing');
         return ({ data: null, error: 'fetchAttendance id missing' });
     }
     const supabase = createClient();
     const { data, error } = await supabase.from('attendance').select('*').eq('id', id).single();
-    if (error) logger.error('supabase:database', `error fetching attendance ${id}...`, error.message)
+    if (error) logger.error('supabase:database', 'attendance.server', `error fetching attendance ${id}...`, error.message)
     return ({ data, error: error?.message });
 };
 
 export const fetchAttendanceByRoomId = async (roomId: number | undefined) => {
     if (!roomId) {
-        logger.error('next:api', 'fetchAttendanceByRoomId id missing');
+        logger.error('next:api', 'attendance.server', 'fetchAttendanceByRoomId id missing');
         return ({ data: null, error: 'fetchAttendanceByrRoomId id missing' });
     }
     const supabase = createClient();
     const { data, error } = await supabase.from('attendance').select('*').eq('room_id', roomId);
-    if (error) logger.error('supabase:database', `error fetching attendance with room ID ${roomId}...`, error.message);
+    if (error) logger.error('supabase:database', 'attendance.server', `error fetching attendance with room ID ${roomId}...`, error.message);
     return ({ data, error: error?.message });
 };
 
 
 export const fetchAttendanceByUser = async (userId: string) => {
     if (!userId) {
-        logger.error('next:api', 'fetchAttendanceByUser id missing');
+        logger.error('next:api', 'attendance.server', 'fetchAttendanceByUser id missing');
         return ({ data: null, error: 'fetchAttendanceByUser id missing' });
     }
     const supabase = createClient();
     const { data, error } = await supabase.from('attendance').select('*').eq('user_id', userId);
-    if (error) logger.error('supabase:database', `error fetching attendance with user ${userId.slice(0, 5)}...`, error.message)
+    if (error) logger.error('supabase:database', 'attendance.server', `error fetching attendance with user ${userId.slice(0, 5)}...`, error.message)
     return ({ data, error: error?.message });
 };
 
 export const fetchNamesFromAttendance = async (userId: string) => {
     const supabase = createClient();
-    const { data, error } = await supabase.from('attendance').select('first_name, last_name').eq('user_id', userId);
+    const { data, error } = await supabase.from('attendance').select('first_name, last_name').eq('user_id', userId).single();
     if (error || null) {
-        logger.log('supabase:database', `no names for user ${userId.slice(0, 5)}...`, error?.message);
+        logger.log('supabase:database', 'attendance.server', 'fetchNamesFromAttendance', `no names for user ${userId.slice(0, 5)}...`, error?.message);
         return ({ data: null, error: error ? error : 'No data found, null returned' });
     } else {
-        logger.log('supabase:database', `fetched names for user ${userId.slice(0, 5)}...`, data[0]?.first_name, data[0]?.last_name);
-        const sendData = { first_name: data[0]?.first_name, last_name: data[0]?.last_name };
-        return ({ data: sendData, error: null });
+        logger.log('supabase:database', 'attendance.server', 'fetchNamesFromAttendance', `fetched names for user ${userId.slice(0, 5)}...`, data?.first_name, data?.last_name);
+        return ({ data, error: null });
     }
 };
 
 export const fetchUserHasSignedAttendance = async (roomId: number, userId: string) => {
-    logger.log('next:api', 'fetUserHasSignedAttendance', `roomId: ${roomId}, userId: ${userId}`);
+    logger.log('next:api', 'attendance.server', 'fetUserHasSignedAttendance', `roomId: ${roomId}, userId: ${userId}`);
 
     const supabase = createClient();
     /*const { data, error } = await supabase.from('attendance').select('*').eq('room_id', roomId);
@@ -78,8 +77,8 @@ export const fetchUserHasSignedAttendance = async (roomId: number, userId: strin
 
     const { data, error } = await supabase.from('attendance').select('id').eq('room_id', roomId).eq('user_id', userId);
 
-    if (error) logger.error('supabase:database', 'fetchUserHasSignedAttendance', `error fetching attendance with room id ${roomId}...`, error.message);
-    if (data && data?.length > 1) logger.warn('supabase:database', 'fetchUserHasSignedAttendance', `more than one attendance found with room id ${roomId}...`, data);
+    if (error) logger.error('supabase:database', 'attendance.server', 'fetchUserHasSignedAttendance', `error fetching attendance with room id ${roomId}...`, error.message);
+    if (data && data?.length > 1) logger.warn('supabase:database', 'attendance.server', 'fetchUserHasSignedAttendance', `more than one attendance found with room id ${roomId}...`, data);
 
     if (data && data?.length > 0) {
         return true
@@ -92,7 +91,7 @@ export async function countAttendances(roomId: string | number) {
     const supabase = createClient()
     const response = await supabase.from('attendance').select('id').eq('room_id', roomId)
     if (response.error) {
-        logger.error('supabase:database', 'countAttendances', 'error counting attendances', response.error.message, 'discord')
+        logger.error('supabase:database', 'attendance.server', 'countAttendances', 'error counting attendances', response.error.message, 'discord')
         return 0
     }
     return response.data.length
