8212eefe3d689d4bdc054ed7702dcb39a863fd08
Johanna <jcourtoi@student.42.fr>
2025-01-15 12:23:45 +0100

    ADD margin for style panel

--- app/(frontend)/[locale]/_components/canvases/custom-ui/CustomTlStylePanel.tsx
+++ app/(frontend)/[locale]/_components/canvases/custom-ui/CustomTlStylePanel.tsx
@@ -5,7 +5,14 @@ export function CustomTlStylePanel(props: TLUiStylePanelProps) {
 	const styles = useRelevantStyles();
 
 	return (
-		<DefaultStylePanel {...props}>
+		<DefaultStylePanel {...props} >
+			<style>
+			{`
+				.tlui-popover__content {
+					margin-left: 4.5rem !important;
+				}
+			`}
+			</style>
 			<DefaultStylePanelContent styles={styles} />
 		</DefaultStylePanel>
 	)
