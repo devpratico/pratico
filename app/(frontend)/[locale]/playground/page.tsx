668ec8cef83c9fe33e3c2d87561716ca9bda3cb7
Johanna <jcourtoi@student.42.fr>
2025-01-16 12:54:01 +0100

    UPDATE playground

--- app/(frontend)/[locale]/playground/page.tsx
+++ app/(frontend)/[locale]/playground/page.tsx
@@ -1,9 +1,6 @@
 "use client";
-import { Box, Container, Section } from "@radix-ui/themes";
-import { Tldraw, TldrawUi, TldrawEditor, DefaultToolbar } from "tldraw";
-import "tldraw/tldraw.css";
-import { CustomTlToolbar } from "../_components/canvases/custom-ui/tool-bar/ToolBar/ToolBar";
 
+import { Container } from "@radix-ui/themes";
 
 export default function PlayGround () {
 	if (process.env.NODE_ENV === 'production') {
@@ -12,22 +9,6 @@ export default function PlayGround () {
 
     return (
 		<Container>
-
-			<Section height='50vh'>
-			<Tldraw
-				forceMobile
-				components={{Toolbar: DefaultToolbar}}
-			/>
-			</Section>
-
-			<Section height='50vh'>
-				<Tldraw>
-					<TldrawEditor >
-						{/* <DefaultToolbar /> */}
-						<TldrawUi/>
-					</TldrawEditor>
-				</Tldraw>
-			</Section>
 		</Container>
 );
 };
