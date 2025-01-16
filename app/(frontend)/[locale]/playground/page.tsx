8bfee4381fb8e515b35a340980ba3b0fe3a9489e
Johanna <jcourtoi@student.42.fr>
2025-01-16 14:19:44 +0100

    UPDATE playgroudn

--- app/(frontend)/[locale]/playground/page.tsx
+++ app/(frontend)/[locale]/playground/page.tsx
@@ -1,15 +1,12 @@
 "use client";
 
-import { Container } from "@radix-ui/themes";
-
 export default function PlayGround () {
 	if (process.env.NODE_ENV === 'production') {
 		return (null);
 	}
 
     return (
-		<Container>
-		</Container>
+		<></>
 );
 };
 
