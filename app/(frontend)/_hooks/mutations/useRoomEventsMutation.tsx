4fa68ca77081b7a4489d05df28ae28361bf44ade
Thomas DOUCHE <33961528+tomx-sh@users.noreply.github.com>
2025-01-22 14:25:40 +0100

    refactor useRoomEventsMutation hook to support multiple event types and remove unused code

--- app/(frontend)/_hooks/mutations/useRoomEventsMutation.tsx
+++ app/(frontend)/_hooks/mutations/useRoomEventsMutation.tsx
@@ -1,25 +1,46 @@
 'use client'
-import { RoomEvent } from "@/app/_types/room-events"
 import { useCallback, useState } from "react"
 import createClient from "@/supabase/clients/client"
 import { TablesInsert } from "@/supabase/types/database.types"
 import { PostgrestError } from "@supabase/supabase-js"
+import { PollUserAnswer } from "@/app/_types/poll"
+import { QuizUserAnswer } from "@/app/_types/quiz"
+import { Json } from "@/supabase/types/database.types"
+
+
+type AddRoomEventArgs = {
+    type: 'start poll'
+    payload: {pollId: string}
+    room_id: string
+} | {
+    type: 'end poll'
+    payload: {startEventId: string, answers: PollUserAnswer[]}
+    room_id: string
+} | {
+    type: 'start quiz'
+    payload: {quizId: string}
+    room_id: string
+} | {
+    type: 'end quiz'
+    payload: {startEventId: string, answers: QuizUserAnswer[]}
+    room_id: string
+}
 
 
 export default function useRoomEventsMutation(): {
-    addRoomEvent: (roomEvent: RoomEvent) => Promise<{ error: PostgrestError | null }>
+    addRoomEvent: (args: AddRoomEventArgs) => Promise<{ error: PostgrestError | null }>
     isPending: boolean
 } {
     const [isPending, setIsPending] = useState(false)
     const supabase = createClient()
 
-    const addRoomEvent = useCallback(async (roomEvent: RoomEvent) => {
+    const addRoomEvent = useCallback(async (args: AddRoomEventArgs) => {
         setIsPending(true)
 
         const newRoomEvent: TablesInsert<'room_events'> = {
-            type: roomEvent.type,
-            payload: roomEvent.payload,
-            room_id: parseInt(roomEvent.room_id)
+            type: args.type,
+            payload: args.payload as Json,
+            room_id: parseInt(args.room_id)
         }
 
         const { error } = await supabase.from('room_events').insert(newRoomEvent)
--- app/_types/room-events.ts
+++
@@ -1,5 +0,0 @@
-export interface RoomEvent {
-    type: string //'start activity' | 'end-activity' 
-    payload: any
-    room_id: string
-}
