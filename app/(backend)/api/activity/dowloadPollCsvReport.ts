0427d03ba289c9976261f18760d40b4270e6ca4e
Thomas DOUCHE <33961528+tomx-sh@users.noreply.github.com>
2025-02-07 14:01:47 +0100

    ADD getPollDates function to fetch poll start and end dates with error handling

--- app/(backend)/api/activity/dowloadPollCsvReport.ts
+++ app/(backend)/api/activity/dowloadPollCsvReport.ts
@@ -1,4 +1,7 @@
 import createClient from "@/supabase/clients/server";
+import DatabaseResponse from "@/app/_utils/DatabaseResponse";
+import logger from "@/app/_utils/logger";
+
 
 async function getTeacherName(userId: string) {
     const supabase = createClient();
@@ -29,6 +32,42 @@ async function getCapsuleTitle(capsuleId: string) {
 
 async function getPollDates(args:{
     startEventId: string,
-}) {
-    //const sup
+}): Promise<DatabaseResponse<{
+    startDate: Date,
+    endDate: Date,
+},  Error
+>> {
+    const supabase = createClient();
+    const {
+        data: startEventData,
+        error: startEventError
+     } = await supabase
+        .from("room_events")
+        .select()
+        .eq("id", args.startEventId)
+        .single();
+
+    if (startEventError) {
+        logger.error(
+            "supabase:database",
+            "downloadPollCsvReport.ts",
+            "getPollDates",
+            "Error fetching start event data",
+            startEventError
+        );
+        return { data: null, error: new Error("Error fetching start event data") };
+    }
+
+
+
+    // Find the corresponding end event.
+    // It is the next event of type end poll in the same room.
+    const endEventData = await supabase
+        .from("room_events")
+        .select()
+        .eq("room_id", startEventData.
+        .eq("type", "end_poll")
+        .gt("created_at", startEventData.created_at)
+        .order("created_at", { ascending: true })
+        .single();
 }
