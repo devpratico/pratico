60b8e46a274beadb39de5011e20ccfc3fa9125d6
Thomas DOUCHE <33961528+tomx-sh@users.noreply.github.com>
2025-02-12 15:55:42 +0100

    ENHANCE asset upload by appending unique ID to file names

--- app/_utils/tldraw/assetStore.ts
+++ app/_utils/tldraw/assetStore.ts
@@ -1,5 +1,5 @@
 "use client"
-import { TLAssetStore } from "tldraw"
+import { TLAssetStore, uniqueId } from "tldraw"
 import uploadCapsuleFile from "../uploadCapsuleFile"
 import logger from "../logger"
 import createClient from "@/supabase/clients/client"
@@ -13,10 +13,17 @@ export default function makeAssetStore(args: {
     return {
         async upload(asset, file) {
 
-            logger.log('tldraw:editor', 'assetStore.ts', 'makeAssetStore', 'Uploading asset', file.name)
+            // Change the file name to append a unique id
+            const fileName = file.name.split('.')
+            const extension = fileName.pop()
+            const newFileName = `${fileName.join('.')}-${uniqueId()}.${extension}`
+            const _file = new File([file], newFileName, { type: file.type })
+
+
+            logger.log('tldraw:editor', 'assetStore.ts', 'makeAssetStore', 'Uploading asset', _file.name)
 
             const { data, error } = await uploadCapsuleFile({
-                file,
+                file: _file,
                 capsuleId: args.capsuleId,
                 folder: 'images'
             })
