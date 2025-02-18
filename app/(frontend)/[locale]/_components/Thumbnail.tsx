5baa73b0d27cf02ae4f3a4c5765ff25b96e4d90e
Johanna <jcourtoi@student.42.fr>
2025-02-18 16:34:01 +0100

    TEST conditionnal pageId

--- app/(frontend)/[locale]/_components/Thumbnail.tsx
+++ app/(frontend)/[locale]/_components/Thumbnail.tsx
@@ -56,8 +56,6 @@ const Thumbnail = ({ snapshot: argSnapshot, scale=0.05, pageId }: ThumbnailProps
     if (!snapshot) {
         return null;
     }
-    if (!pageId)
-        return null;
     /**
      * When using the hookSnapshot, it may not contain the desired pageId yet,
      * because we debounce its updates for performance reasons.
