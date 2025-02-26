6824da36e4fd765d12dec2a6ad681c4f62deb7cd
Johanna <jcourtoi@student.42.fr>
2025-02-26 15:39:31 +0100

    RM migrations for relation room_id attendance and ADD it back

--- supabase/migrations/20250210132627_add_relation_attendance_room_id_and_room_table.sql
+++
@@ -1,5 +0,0 @@
-alter table "public"."attendance" add constraint "attendance_room_id_fkey" FOREIGN KEY (room_id) REFERENCES rooms(id) not valid;
-
-alter table "public"."attendance" validate constraint "attendance_room_id_fkey";
-
-
--- supabase/migrations/20250226143842_add_relation_cascade_room_id_in_attendance_table.sql
+++ supabase/migrations/20250226143842_add_relation_cascade_room_id_in_attendance_table.sql
@@ -1,5 +1,3 @@
-alter table "public"."attendance" drop constraint "attendance_room_id_fkey";
-
 alter table "public"."attendance" add constraint "attendance_room_id_fkey" FOREIGN KEY (room_id) REFERENCES rooms(id) ON UPDATE CASCADE ON DELETE CASCADE not valid;
 
 alter table "public"."attendance" validate constraint "attendance_room_id_fkey";
