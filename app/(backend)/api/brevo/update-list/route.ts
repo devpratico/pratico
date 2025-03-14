9d3b527a1b1e51b639f5bf454d6f631b52ab0bfa
Thomas DOUCHE <33961528+tomx-sh@users.noreply.github.com>
2025-03-14 11:50:52 +0100

    add Brevo API to manage contacts

--- app/(backend)/api/brevo/update-list/route.ts
+++ app/(backend)/api/brevo/update-list/route.ts
@@ -1,10 +1,79 @@
+import logger from "@/app/_utils/logger";
 
 
 export async function POST(req: Request) {
     console.log('req', req);
     const { type, record } = await req.json();
 
-    console.log(type, record);
+    logger.log(
+        "next:api",
+        "brevo/update-list",
+        "POST",
+        "type",
+        type,
+        "record",
+        record
+    )
+
+    const BREVO_API_KEY = process.env.BREVO;
+    if (!BREVO_API_KEY) {
+        return Response.json({
+            success: false,
+            message: 'Brevo API key not found',
+        });
+    }
+
+    if (type === 'INSERT') {
+        const options = {
+            method: 'POST',
+            headers: {
+                accept: 'application/json',
+                'content-type': 'application/json',
+                'api-key': BREVO_API_KEY
+            },
+            body: JSON.stringify({ updateEnabled: false })
+        };
+
+        fetch('https://api.brevo.com/v3/contacts', options)
+            .then(res => res.json())
+            .then(res => console.log(res))
+            .catch(err => console.error(err));
+
+    } else if (type === 'UPDATE') {
+        const options = {
+            method: 'PUT',
+            headers: {
+                accept: 'application/json',
+                'content-type': 'application/json',
+                'api-key': BREVO_API_KEY
+            }
+        };
+
+        fetch('https://api.brevo.com/v3/contacts/identifier', options)
+            .then(res => res.json())
+            .then(res => console.log(res))
+            .catch(err => console.error(err));
+
+    } else if (type === 'DELETE') {
+        const options = {
+            method: 'DELETE',
+            headers: {
+                accept: 'application/json',
+                'api-key': BREVO_API_KEY
+            }
+        };
+
+        fetch('https://api.brevo.com/v3/contacts/identifier', options)
+            .then(res => res.json())
+            .then(res => console.log(res))
+            .catch(err => console.error(err));
+
+    } else {
+        return Response.json({
+            success: false,
+            message: 'Invalid type',
+        });
+    }
 
     return Response.json({
         success: true,
