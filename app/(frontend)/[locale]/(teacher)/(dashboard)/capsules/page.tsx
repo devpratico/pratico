ffe024942dc9772b1b2868e5d736cc2f89da35a9
Thomas DOUCHE <33961528+tomx-sh@users.noreply.github.com>
2025-03-18 15:18:13 +0100

    conditionnaly show banner and link to subscribe page

--- app/(frontend)/[locale]/(teacher)/(dashboard)/capsules/page.tsx
+++ app/(frontend)/[locale]/(teacher)/(dashboard)/capsules/page.tsx
@@ -5,6 +5,8 @@ import { fetchCapsulesDataAndRoomStatus } from "@/app/(backend)/api/capsule/caps
 import { CapsulesDisplay } from "./_components/CapsulesDiplay";
 import { CapsuleType } from "../reports/page";
 import logger from "@/app/_utils/logger";
+import { customerIsSubscribed } from "@/app/(backend)/api/stripe/stripe.server";
+import LinkButton from "../../../_components/LinkButton";
 
 export type ExtendedCapsuleType = CapsuleType & { roomOpen: boolean, roomCode: string | null | undefined }
 export default async function Page() {
@@ -20,43 +22,13 @@ export default async function Page() {
             capsules = data;
     }
 
+    const isSubscribed = await customerIsSubscribed(user?.id);
+
     return (
         <ScrollArea>
             <Container pr='3' pl={{ initial: '3', xs: '0' }}>
 
-                <Box mt='3'>
-                    <Card>
-                        <Grid columns='2' gap='3'>
-                            <Callout.Root color='crimson'>
-                                <Callout.Icon>
-                                    <AlertTriangle />
-                                </Callout.Icon>
-                                <Callout.Text>
-                                    Vous êtes limité à 10 participants maximum par session.
-                                </Callout.Text>
-                            </Callout.Root>
-
-                            <Flex direction='column' justify='center' gap='3' p='3' style={{
-                                backgroundColor: 'var(--yellow-a3)',
-                                borderRadius: 'var(--radius-3)',
-                                color: 'var(--yellow-11)',
-                            }}>
-                                <Button color='yellow'><Star/>Passer à Pratico Pro</Button>
-                                <Text size='2' align='center'>
-                                    Pour un nombre illimité de participants.
-                                </Text>
-                                <Text size='1' align='center' color='yellow'>
-                                    Vous êtes une entreprise ?&nbsp;
-                                    <Link href="test.com">Contactez-nous</Link>
-                                </Text>
-                            </Flex>
-                        </Grid>
-
-                        
-
-                    </Card>
-                </Box>
-
+                { !isSubscribed && <Banner /> }
 
                 <Section>
                     <CapsulesDisplay capsules={capsules} />
@@ -67,3 +39,45 @@ export default async function Page() {
 }
 
 
+function Banner() {
+    return (
+        <Box mt='3'>
+            <Card>
+                <Grid columns='2' gap='3'>
+                    <Callout.Root color='crimson'>
+                        <Callout.Icon>
+                            <AlertTriangle />
+                        </Callout.Icon>
+                        <Callout.Text>
+                            Vous êtes limité à 10 participants maximum par session.
+                        </Callout.Text>
+                    </Callout.Root>
+
+                    <Flex direction='column' justify='center' gap='3' p='3' style={{
+                        backgroundColor: 'var(--yellow-a3)',
+                        borderRadius: 'var(--radius-3)',
+                        color: 'var(--yellow-11)',
+                    }}>
+                        <LinkButton
+                            color='yellow'
+                            href="/subscribe"
+                            target='_blank'
+                        >
+                            <Star />Passer à Pratico Pro
+                        </LinkButton>
+                        <Text size='2' align='center'>
+                            Pour un nombre illimité de participants.
+                        </Text>
+                        <Text size='1' align='center' color='yellow'>
+                            Vous êtes une entreprise ?&nbsp;
+                            <Link href="mailto:bonjour@pratico.live">Contactez-nous</Link>
+                        </Text>
+                    </Flex>
+                </Grid>
+
+
+
+            </Card>
+        </Box>
+    )
+}
