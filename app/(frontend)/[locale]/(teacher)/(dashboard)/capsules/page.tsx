34ecb3802759a8156ab819ec2fa688e6f40a64d3
Thomas DOUCHE <33961528+tomx-sh@users.noreply.github.com>
2025-03-18 15:08:43 +0100

    add a banner

--- app/(frontend)/[locale]/(teacher)/(dashboard)/capsules/page.tsx
+++ app/(frontend)/[locale]/(teacher)/(dashboard)/capsules/page.tsx
@@ -1,5 +1,5 @@
-import { ScrollArea, Container, Section, Flex, Box, IconButton } from "@radix-ui/themes"
-import { Search } from "lucide-react"
+import { ScrollArea, Container, Section, Flex, Box, IconButton, Grid, Callout, Card, Button, Link, Text } from "@radix-ui/themes"
+import { AlertTriangle, Star, Gem } from "lucide-react"
 import { fetchUser } from "@/app/(backend)/api/user/user.server";
 import { fetchCapsulesDataAndRoomStatus } from "@/app/(backend)/api/capsule/capsule.server";
 import { CapsulesDisplay } from "./_components/CapsulesDiplay";
@@ -23,35 +23,43 @@ export default async function Page() {
     return (
         <ScrollArea>
             <Container pr='3' pl={{ initial: '3', xs: '0' }}>
+
+                <Box mt='3'>
+                    <Card>
+                        <Grid columns='2' gap='3'>
+                            <Callout.Root color='crimson'>
+                                <Callout.Icon>
+                                    <AlertTriangle />
+                                </Callout.Icon>
+                                <Callout.Text>
+                                    Vous êtes limité à 10 participants maximum par session.
+                                </Callout.Text>
+                            </Callout.Root>
+
+                            <Flex direction='column' justify='center' gap='3' p='3' style={{
+                                backgroundColor: 'var(--yellow-a3)',
+                                borderRadius: 'var(--radius-3)',
+                                color: 'var(--yellow-11)',
+                            }}>
+                                <Button color='yellow'><Star/>Passer à Pratico Pro</Button>
+                                <Text size='2' align='center'>
+                                    Pour un nombre illimité de participants.
+                                </Text>
+                                <Text size='1' align='center' color='yellow'>
+                                    Vous êtes une entreprise ?&nbsp;
+                                    <Link href="test.com">Contactez-nous</Link>
+                                </Text>
+                            </Flex>
+                        </Grid>
+
+                        
+
+                    </Card>
+                </Box>
+
+
                 <Section>
-
-                    <Flex gap='3' align='center'>
-
-                        {/* <SegmentedControl.Root defaultValue='grid'>
-                            <SegmentedControl.Item value='grid'>
-                                <Flex><LayoutGrid size='18' /></Flex>
-                            </SegmentedControl.Item>
-
-                            <SegmentedControl.Item value='list'>
-                                <Flex><List size='18' /></Flex>
-                            </SegmentedControl.Item>
-                        </SegmentedControl.Root>
-
-                        <Box display={{ initial: 'none', md: 'block' }}>
-                            <TextField.Root placeholder='Rechercher' disabled>
-                                <TextField.Slot>
-                                    <Search size='18' />
-                                </TextField.Slot>
-                            </TextField.Root>
-                        </Box> */}
-
-                        <Box display={{ initial: 'block', md: 'none' }}>
-                            <IconButton variant='ghost'><Search size='18' /></IconButton>
-                        </Box>
-
-                    </Flex>
                     <CapsulesDisplay capsules={capsules} />
-
                 </Section>
             </Container>
         </ScrollArea>
